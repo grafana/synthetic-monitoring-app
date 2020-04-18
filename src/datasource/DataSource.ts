e8c152cc4202763eba71ec9725ebc6e3a43876ab
Ryan McKinley <ryantxu@gmail.com>
2020-04-17 18:28:24 -0700

    routes

--- src/datasource/DataSource.ts
+++ src/datasource/DataSource.ts
@@ -12,21 +12,19 @@ export class DataSource extends DataSourceApi<WorldpingQuery, WorldpingOptions> 
   }
 
   async query(options: DataQueryRequest<WorldpingQuery>): Promise<DataQueryResponse> {
-    const { range } = options;
-    const from = range!.from.valueOf();
-    const to = range!.to.valueOf();
-
-    console.log(this.instanceSettings, options);
+    console.log('QUERY', this.instanceSettings, options);
 
     return getBackendSrv()
       .datasourceRequest({
-        url: `${this.instanceSettings.url}/dev/x`,
+        method: 'GET',
+        url: `${this.instanceSettings.url}/dev/v1/probe/list`,
       })
       .then((res: any) => {
+        console.log('GOT', res);
         const frame = new MutableDataFrame({
           refId: 'A',
           fields: [
-            { name: 'Time', values: [from, to], type: FieldType.time },
+            { name: 'Time', values: [1, 2], type: FieldType.time },
             { name: 'Value', values: [7, 8], type: FieldType.number },
           ],
         });
@@ -35,8 +33,7 @@ export class DataSource extends DataSourceApi<WorldpingQuery, WorldpingOptions> 
   }
 
   async testDatasource() {
-
-    console.log( 'TEST', this.instanceSettings );
+    console.log('TEST', this.instanceSettings);
 
     // Implement
     return this.query({
