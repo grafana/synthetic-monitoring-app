c76b8b67dfdfae7bdf7a34d8dec9c96068188787
woodsaj <awoods@grafana.com>
2020-06-16 20:36:19 +0000

    ui updates

--- src/components/UptimeGauge.tsx
+++ src/components/UptimeGauge.tsx
@@ -1,49 +1,62 @@
 import React, { PureComponent } from 'react';
-import { BigValueColorMode, BigValueGraphMode, BigValue, Container } from '@grafana/ui';
+import { BigValueColorMode, BigValueGraphMode, BigValue, Container, IconName, Icon } from '@grafana/ui';
 import { DataSourceInstanceSettings, GraphSeriesValue, DisplayValue } from '@grafana/data';
 import { config, getBackendSrv } from '@grafana/runtime';
-
-interface GaugeData {
-  value: DisplayValue;
-  points: GraphSeriesValue[][];
-}
+import { Check } from 'types';
 
 interface Props {
-  probe: string;
   ds: DataSourceInstanceSettings;
+  labelNames: string[];
+  labelValues: string[];
+  height: number;
+  width: number;
+  sparkline: boolean;
+  onClick?: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
 }
 
 interface State {
-  data: GaugeData;
+  filter?: string;
+  value: DisplayValue;
+  points: GraphSeriesValue[][];
   lastUpdate: number;
 }
 
-export class ProbeUptime extends PureComponent<Props, State> {
+export class UptimeGauge extends PureComponent<Props, State> {
   state: State = {
     lastUpdate: 0,
-    data: {
-      value: {
-        numeric: 0,
-        title: 'Uptime',
-        text: 'loading...',
-      },
-      points: [],
+    value: {
+      numeric: 0,
+      title: 'Uptime',
+      text: 'loading...',
     },
+    points: [],
   };
 
   async componentDidMount() {
-    this.queryUptime();
+    const { labelNames, labelValues } = this.props;
+    let filters: string[] = [];
+    for (let i in labelNames) {
+      const k = labelNames[i];
+      const v = labelValues[i];
+      filters.push(`${k}="${v}"`);
+    }
+    const filter = filters.join(',');
+    this.setState({ filter }, this.queryUptime);
   }
 
   async queryUptime() {
-    const { ds, probe } = this.props;
-    const backendSrv = await getBackendSrv();
+    const { ds, sparkline } = this.props;
+    const { filter } = this.state;
+    if (!filter) {
+      return;
+    }
+    const backendSrv = getBackendSrv();
     const lastUpdate = Math.floor(Date.now() / 1000);
     const resp = await backendSrv.datasourceRequest({
       method: 'GET',
       url: `${ds.url}/api/v1/query`,
       params: {
-        query: `sum(sum_over_time(probe_success{probe="${probe}"}[24h])) / sum(count_over_time(probe_success{probe="${probe}"}[24h]))`,
+        query: `sum(rate(probe_success_sum{${filter}}[3h])) / sum(rate(probe_success_count{${filter}}[3h]))`,
         time: lastUpdate,
       },
     });
@@ -51,10 +64,14 @@ export class ProbeUptime extends PureComponent<Props, State> {
       console.log(resp);
       return;
     }
-    let { data } = this.state;
+    let value: DisplayValue = {
+      numeric: 0,
+      title: 'Uptime',
+      text: 'loading...',
+    };
     if (resp.data.data.result.length < 1) {
-      data.value.text = 'N/A';
-      this.setState({ data, lastUpdate });
+      value.text = 'N/A';
+      this.setState({ value, lastUpdate });
       return;
     }
     const uptime = parseFloat(resp.data.data.result[0].value[1]) * 100;
@@ -62,24 +79,31 @@ export class ProbeUptime extends PureComponent<Props, State> {
     if (uptime < 99) {
       color = 'red';
     }
-    data.value.color = color;
-    data.value.numeric = uptime;
-    data.value.text = uptime.toFixed(2) + '%';
+    value.color = color;
+    value.numeric = uptime;
+    value.text = uptime.toFixed(2) + '%';
 
-    this.setState({ data, lastUpdate });
-    this.queryHistory();
+    this.setState({ value, lastUpdate }, () => {
+      if (sparkline) {
+        this.queryHistory();
+      }
+    });
   }
 
   async queryHistory() {
-    const { ds, probe } = this.props;
-    const backendSrv = await getBackendSrv();
+    const { ds } = this.props;
+    const { filter } = this.state;
+    if (!filter) {
+      return;
+    }
+    const backendSrv = getBackendSrv();
     const lastUpdate = this.state.lastUpdate + 1;
     const resp = await backendSrv.datasourceRequest({
       method: 'POST',
       url: `${ds.url}/api/v1/query_range`,
       params: {
-        query: `100 * sum(sum_over_time(probe_success{probe="${probe}"}[10m])) / sum(count_over_time(probe_success{probe="${probe}"}[10m]))`,
-        start: lastUpdate - 60 * 60 * 24,
+        query: `100 * sum(rate(probe_success_sum{${filter}}[10m])) / sum(rate(probe_success_count{${filter}}[10m]))`,
+        start: lastUpdate - 60 * 60 * 3,
         end: lastUpdate,
         step: 600,
       },
@@ -88,42 +112,111 @@ export class ProbeUptime extends PureComponent<Props, State> {
       console.log(resp);
       return;
     }
-    let { data } = this.state;
+    let points: GraphSeriesValue[][] = [];
     if (resp.data.data.result.length < 1) {
       console.log(resp.data);
       return;
     }
-    let points: GraphSeriesValue[][] = [];
     let i = 0;
     for (const p of resp.data.data.result[0].values) {
       points.push([i, parseFloat(p[1])]);
       i++;
     }
-    data.points = points;
-    console.log(data.points);
-    this.setState({ data: data, lastUpdate: lastUpdate + 1 });
+    this.setState({ points: points, lastUpdate: lastUpdate + 1 });
   }
 
   render() {
-    const { data } = this.state;
-    const sparkline = {
-      yMin: 0,
-      yMax: 200,
-      data: data.points,
-    };
-
+    const { value, points } = this.state;
+    const { height, width, sparkline, onClick } = this.props;
+    let sparklineData = undefined;
+    if (sparkline && points.length > 0) {
+      sparklineData = {
+        yMin: 0,
+        yMax: 150,
+        data: points,
+      };
+    }
     return (
       <Container>
         <BigValue
           theme={config.theme}
           colorMode={BigValueColorMode.Value}
-          height={200}
-          width={300}
+          height={height}
+          width={width}
           graphMode={BigValueGraphMode.Area}
-          value={data.value}
-          sparkline={sparkline}
+          value={value}
+          sparkline={sparklineData}
+          onClick={onClick}
         />
       </Container>
     );
+  }
+}
+
+interface CheckHealthProps {
+  ds: DataSourceInstanceSettings;
+  check: Check;
+}
+
+interface CheckHealthState {
+  iconName: IconName;
+  className: string;
+}
+
+export class CheckHealth extends PureComponent<CheckHealthProps, CheckHealthState> {
+  state: CheckHealthState = {
+    iconName: 'heart',
+    className: 'paused',
+  };
+
+  async componentDidMount() {
+    const { check } = this.props;
+    if (!check.enabled) {
+      this.setState({ iconName: 'pause' });
+      return;
+    }
+    await this.queryUptime();
+  }
+
+  async queryUptime() {
+    const { ds, check } = this.props;
+    const filter = `instance="${check.target}", job="${check.job}"`;
+
+    const backendSrv = getBackendSrv();
+    const lastUpdate = Math.floor(Date.now() / 1000);
+    const resp = await backendSrv.datasourceRequest({
+      method: 'GET',
+      url: `${ds.url}/api/v1/query`,
+      params: {
+        query: `sum(probe_success{${filter}}) / count(probe_success{${filter}})`,
+        time: lastUpdate,
+      },
+    });
+    if (!resp.ok) {
+      console.log(resp);
+      return;
+    }
+    let iconName: IconName = 'heart';
+    let className = 'ok';
+    if (resp.data.data.result.length < 1) {
+      iconName = 'question-circle';
+      className = 'paused';
+      this.setState({ iconName, className });
+      return;
+    }
+    const uptime = parseFloat(resp.data.data.result[0].value[1]) * 100;
+    if (uptime < 99) {
+      className = 'warning';
+    }
+    if (uptime < 50) {
+      className = 'critical';
+      iconName = 'heart-break';
+    }
+    this.setState({ iconName, className });
+  }
+
+  render() {
+    const { iconName, className } = this.state;
+    return <Icon name={iconName} size="xxl" className={`alert-rule-item__icon alert-state-${className}`} />;
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import { DataSourceInstanceSettings, DataSourceSettings } from '@grafana/data';
 
-import { WorldpingOptions } from './datasource/types';
+import { WorldpingOptions, DashboardInfo } from './datasource/types';
 
 import { config, getBackendSrv } from '@grafana/runtime';
 import {
@@ -14,6 +14,7 @@ import {
   DnsRecordType,
   DnsProtocol,
 } from 'types';
+import { WorldPingDataSource } from 'datasource/DataSource';
 
 /**
  * Find all worldping datasources
@@ -155,4 +156,16 @@ export function defaultSettings(t: CheckType): Settings | undefined {
       };
     }
   }
+}
+
+/** Given hosted info, link to an existing instance */
+export function dashboardUID(checkType: string, ds: WorldPingDataSource): DashboardInfo | undefined {
+  const dashboards = ds.instanceSettings.jsonData.dashboards;
+  let target: DashboardInfo | undefined = undefined;
+  for (const item of dashboards) {
+    if (item.title.toLocaleLowerCase() === 'worldping ' + checkType) {
+      target = item;
+    }
+  }
+  return target;
 }
