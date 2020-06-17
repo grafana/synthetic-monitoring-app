a6e15e5d027253b7dd27745de2779968cc55abc2
CircleCI Automation <eng@grafana.com>
2020-06-17 13:05:53 +0000

    automated release 0.0.1 [skip ci]

--- src/datasource/module.ts
+++
@@ -1,9 +0,0 @@
-import { DataSourcePlugin } from '@grafana/data';
-import { WorldPingDataSource } from './DataSource';
-import { ConfigEditor } from './ConfigEditor';
-import { QueryEditor } from './QueryEditor';
-import { WorldpingQuery, WorldpingOptions } from './types';
-
-export const plugin = new DataSourcePlugin<WorldPingDataSource, WorldpingQuery, WorldpingOptions>(WorldPingDataSource)
-  .setConfigEditor(ConfigEditor)
-  .setQueryEditor(QueryEditor);
