dbdae2ecee986be2082c87c9675bbe479bdfc5a2
Russ <8377044+rdubrock@users.noreply.github.com>
2020-08-20 12:24:33 -0400

    chore: add tests for CheckList

--- src/components/TenantSetup.tsx
+++ src/components/TenantSetup.tsx
@@ -41,7 +41,6 @@ export class TenantSetup extends PureComponent<Props, State> {
 
   onInit = async ({ apiHost = DEFAULT_API_HOST, adminApiToken }: InitParams) => {
     const { instance } = this.props;
-    console.log('API HOST', apiHost);
     const info = await instance.registerInit(apiHost, adminApiToken).catch(err => {
       console.error('failed to init. ', err);
       if (err.data.msg) {
