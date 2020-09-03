6e00324bbb5ba04d1bb80700a1f89723ad031dd1
Russ <8377044+rdubrock@users.noreply.github.com>
2020-09-03 11:53:18 -0700

    feat: refresh metric data on an interval (#88)
    * feat: refresh metric data on an interval
    
    * chore: move refetching to single effect hook

--- src/hooks/useMetricData.ts
+++ src/hooks/useMetricData.ts
@@ -6,27 +6,36 @@ interface UseMetricOptions {
   skip?: boolean;
 }
 
-export function useMetricData(query: string, options?: (MetricQueryOptions & UseMetricOptions) | undefined) {
+const REFETCH_INTERVAL = 10000;
+
+export function useMetricData(query: string, options?: MetricQueryOptions & UseMetricOptions) {
   const { instance } = useContext(InstanceContext);
   const [isFetchingData, setIsFetchingData] = useState(true);
   const [data, setData] = useState<any[]>([]);
   const [error, setError] = useState<string | undefined>();
 
+  // refresh data every 60 seconds
   useEffect(() => {
     const getData = async () => {
-      setIsFetchingData(true);
       const url = instance?.api?.getMetricsDS()?.url;
-      if (!url) {
+      if (options?.skip || !url) {
         return;
       }
+      setIsFetchingData(true);
       const { error: queryError, data: queryData } = await queryMetric(url, query, options);
+
       setData(queryData);
       setError(queryError);
       setIsFetchingData(false);
     };
-    if (!options?.skip) {
+
+    getData();
+
+    const interval = setInterval(() => {
       getData();
-    }
+    }, REFETCH_INTERVAL);
+
+    return () => clearInterval(interval);
   }, [query, instance?.api, options]);
 
   return { loading: isFetchingData, data, error };
