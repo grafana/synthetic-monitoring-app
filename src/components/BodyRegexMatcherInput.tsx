da4ef818a6d4cb55838791ad078c4846a6032895
Russ <8377044+rdubrock@users.noreply.github.com>
2020-09-15 17:32:16 -0700

    chore: add some padding to BodyRegexMatcher

--- src/components/BodyRegexMatcherInput.tsx
+++ src/components/BodyRegexMatcherInput.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { useFormContext, useFieldArray } from 'react-hook-form';
-import { Field, Input, IconButton, HorizontalGroup, Icon, Button } from '@grafana/ui';
+import { css } from 'emotion';
+import { Field, Input, IconButton, HorizontalGroup, Icon, Button, useTheme } from '@grafana/ui';
 
 interface Props {
   isEditor: boolean;
@@ -12,18 +13,42 @@ interface Props {
 export const BodyRegexMatcherInput: FC<Props> = ({ isEditor, name, label, description }) => {
   const { control, register } = useFormContext();
   const { fields, append, remove } = useFieldArray({ control, name });
+  const theme = useTheme();
   return (
     <Field label={label} description={description} disabled={!isEditor}>
       <div>
-        {fields.map((field, index) => (
-          <HorizontalGroup key={field.id}>
-            <Input ref={register()} type="text" name={`${name}[${index}]`} placeholder="regexp" disabled={!isEditor} />
-            <IconButton name="minus-circle" type="button" disabled={!isEditor} onClick={() => remove(index)} />
-          </HorizontalGroup>
-        ))}
+        <div
+          className={css`
+            margin-bottom: ${theme.spacing.sm};
+            display: flex;
+            flex-direction: column;
+          `}
+        >
+          {fields.map((field, index) => (
+            <div
+              key={field.id}
+              className={css`
+                margin-bottom: ${theme.spacing.sm};
+              `}
+            >
+              <HorizontalGroup>
+                <Input
+                  ref={register()}
+                  type="text"
+                  name={`${name}[${index}]`}
+                  placeholder="regexp"
+                  disabled={!isEditor}
+                />
+                <IconButton name="minus-circle" type="button" disabled={!isEditor} onClick={() => remove(index)} />
+              </HorizontalGroup>
+            </div>
+          ))}
+        </div>
         <Button type="button" onClick={() => append({ value: '' })} disabled={!isEditor} variant="secondary" size="sm">
-          <Icon name="plus" />
-          &nbsp; Add Body Regexp
+          <HorizontalGroup spacing="sm">
+            <Icon name="plus" />
+            Add Body Regexp
+          </HorizontalGroup>
         </Button>
       </div>
     </Field>
