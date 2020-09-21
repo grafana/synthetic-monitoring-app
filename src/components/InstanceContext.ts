745582d824b6787b79ef42f0a97b97ba91a58c0f
CircleCI Automation <eng@grafana.com>
2020-09-21 20:38:52 +0000

    automated release 0.0.11 [skip ci]

--- src/components/InstanceContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-import { GrafanaInstances } from 'types';
-
-interface InstanceContextValue {
-  loading: boolean;
-  instance: GrafanaInstances | undefined;
-}
-
-export const InstanceContext = createContext<InstanceContextValue>({ instance: undefined, loading: true });
