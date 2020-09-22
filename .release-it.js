57487d2a5cdb518c329d158d83a008e0e617b521
Russ <8377044+rdubrock@users.noreply.github.com>
2020-09-22 10:53:17 -0700

    chore: add changelog generation

--- .release-it.js
+++ .release-it.js
@@ -1,8 +1,21 @@
 module.exports = {
   hooks: {
-    'before:init': ['yarn build'],
+    'before:init': ['yarn build', 'yarn test'],
+  },
+  git: {
+    commitMessage: 'chore: release v${version}',
+    tag: false,
+  },
+  github: {
+    release: false,
   },
   npm: {
     publish: false,
+  },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      preset: 'angular',
+      infile: 'CHANGELOG.md',
+    },
   },
 };
