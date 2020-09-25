86dfdef18eabf988f975510ae2001c38c14f01b0
Russ <8377044+rdubrock@users.noreply.github.com>
2020-09-25 09:41:42 -0700

    chore: filter versioning by git tag

--- .release-it.js
+++ .release-it.js
@@ -1,10 +1,10 @@
 module.exports = {
   // TODD: enable the before init hooks once grafana/toolkit can limit the jest workers
-  // hooks: {
-  //   'before:init': ['yarn build'],
-  // },
+  hooks: {
+    'before:init': ['yarn build'],
+  },
   git: {
-    commitMessage: 'chore: release v${version} [skip ci]',
+    commitMessage: 'chore: release v${version}',
   },
   github: {
     release: false,
