b4c739b52f9c21848c75b1980d61ffbc95d271cd
Russ <8377044+rdubrock@users.noreply.github.com>
2020-09-25 12:11:58 -0700

    chore: add tests for check page

--- src/page/ChecksPage.test.tsx
+++ src/page/ChecksPage.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import { ChecksPage } from './ChecksPage';
 import { getInstanceMock } from 'datasource/__mocks__/DataSource';
+import userEvent from '@testing-library/user-event';
+jest.setTimeout(20000);
 
 interface RenderArgs {
   checkId?: string;
@@ -12,7 +14,19 @@ const renderChecksPage = ({ checkId }: RenderArgs = {}) => {
   render(<ChecksPage instance={{ api: instance }} id={checkId} />);
 };
 
-test('renders check editor existing check', () => {
+test('renders checks', () => {
   renderChecksPage();
+  waitFor(() => expect(screen.getByText('a jobname')).toBeInTheDocument());
+});
+
+test('renders check editor new check', async () => {
+  renderChecksPage();
+  await waitFor(() => screen.getByRole('button', { name: 'New Check' }));
+  userEvent.click(screen.getByRole('button', { name: 'New Check' }));
+  expect(await screen.findByText('Add Check')).toBeInTheDocument();
+});
+
+test('renders check editor existing check', () => {
+  renderChecksPage({ checkId: '1' });
   waitFor(() => expect(screen.getByText('Edit Check')).toBeInTheDocument());
 });
