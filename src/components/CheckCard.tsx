d187453e1318d7b7c61764625875b7aeb328e062
Russ <8377044+rdubrock@users.noreply.github.com>
2021-02-17 12:38:12 -0900

    refactor: add styles to check card

--- src/components/CheckCard.tsx
+++ src/components/CheckCard.tsx
@@ -1,14 +1,15 @@
 import { getLocationSrv } from '@grafana/runtime';
 import React, { useContext } from 'react';
+import appEvents from 'grafana/app/core/app_events';
 import { CheckHealth } from 'components/CheckHealth';
 import { UptimeGauge } from 'components/UptimeGauge';
 import { checkType as getCheckType, dashboardUID } from 'utils';
 // Types
 import { Check, CheckType, Label } from 'types';
-import { Button, IconButton, HorizontalGroup, VerticalGroup, Container, useStyles, Checkbox } from '@grafana/ui';
+import { Button, IconButton, HorizontalGroup, VerticalGroup, useStyles, Checkbox, ButtonGroup } from '@grafana/ui';
 import { css } from 'emotion';
 import { InstanceContext } from './InstanceContext';
-import { GrafanaTheme } from '@grafana/data';
+import { AppEvents, GrafanaTheme } from '@grafana/data';
 import { calculateUsage } from 'checkUsageCalc';
 
 interface Props {
@@ -18,6 +19,8 @@ interface Props {
 
 const getStyles = (theme: GrafanaTheme) => ({
   cardWrapper: css`
+    display: flex;
+    flex-direction: row;
     background-color: ${theme.colors.bg2};
     border: 1px solid #343b40;
     border-radius: 2px;
@@ -26,6 +29,33 @@ const getStyles = (theme: GrafanaTheme) => ({
     cursor: pointer;
     margin-bottom: ${theme.spacing.sm};
   `,
+  checkbox: css`
+    margin-right: ${theme.spacing.sm};
+    display: flex;
+    align-items: flex-start;
+  `,
+  body: css`
+    display: flex;
+    flex-direction: row;
+    flex-grow: 1;
+    border-right: 1px solid #343b40;
+    padding-right: ${theme.spacing.md};
+  `,
+  checkInfoContainer: css`
+    flex-grow: 1;
+  `,
+  checkTarget: css`
+    font-size: ${theme.typography.size.sm};
+    line-height: ${theme.typography.lineHeight.sm};
+    font-weight: ${theme.typography.weight.bold};
+    margin-bottom: ${theme.spacing.sm};
+  `,
+  checkDetails: css`
+    font-size: ${theme.typography.size.sm};
+    line-height: ${theme.typography.lineHeight.sm};
+    margin-bottom: ${theme.spacing.sm};
+  `,
+  stats: css``,
 });
 
 export const CheckCard = ({ check, onLabelSelect }: Props) => {
@@ -37,7 +67,7 @@ export const CheckCard = ({ check, onLabelSelect }: Props) => {
     const target = dashboardUID(checkType, instance.api);
 
     if (!target) {
-      console.log('dashboard not found.', checkType);
+      appEvents.emit(AppEvents.alertError, ['Dashboard not found']);
       return;
     }
 
@@ -50,8 +80,6 @@ export const CheckCard = ({ check, onLabelSelect }: Props) => {
       },
     });
   };
-
-  console.log(check, checkType);
 
   const usage = calculateUsage({
     probeCount: check.probes.length,
@@ -69,65 +97,60 @@ export const CheckCard = ({ check, onLabelSelect }: Props) => {
       className={styles.cardWrapper}
       aria-label="check-card"
     >
-      <HorizontalGroup justify="space-between">
-        <div className="card-item-body">
-          <Checkbox />
-          <VerticalGroup>
-            <div className="card-item-name">{check.job}</div>
-            <div className="card-item-sub-name">{check.target}</div>
-            <div>
-              <span>{checkType} |</span>
-              <span>{check.frequency / 1000}s frequency |</span>
-              <span>
-                {usage.activeSeries}
-                &nbsp;active series
-              </span>
-            </div>
-            <div>
-              {check.labels.map((label: Label, index) => (
-                <Button
-                  variant="secondary"
-                  key={index}
-                  className={css`
-                    border: none;
-                    background: inherit;
-                  `}
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    onLabelSelect(label);
-                  }}
-                  type="button"
-                >
-                  {label.name}={label.value}
-                </Button>
-              ))}
-            </div>
-          </VerticalGroup>
-          <HorizontalGroup>
-            <IconButton
-              name="pen"
-              onClick={() => {
-                getLocationSrv().update({
-                  partial: true,
-                  query: {
-                    id: check.id,
-                  },
-                });
-              }}
-            />
-            <IconButton name="trash-alt" />
-          </HorizontalGroup>
+      <div className={styles.checkbox}>
+        <Checkbox />
+      </div>
+      <div className={styles.body}>
+        <div className={styles.checkInfoContainer}>
+          <h3>{check.job}</h3>
+          <div className={styles.checkTarget}>{check.target}</div>
+          <div className={styles.checkDetails}>
+            {checkType} | {check.frequency / 1000}s frequency | {usage.activeSeries} active series
+          </div>
+          <div>
+            {check.labels.map((label: Label, index) => (
+              <Button
+                variant="secondary"
+                key={index}
+                className={css`
+                  border: none;
+                  background: inherit;
+                `}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onLabelSelect(label);
+                }}
+                type="button"
+              >
+                {label.name}={label.value}
+              </Button>
+            ))}
+          </div>
         </div>
-        <HorizontalGroup justify="flex-end">
-          <UptimeGauge
-            labelNames={['instance', 'job']}
-            labelValues={[check.target, check.job]}
-            height={70}
-            width={150}
-            sparkline={false}
+        <ButtonGroup>
+          <IconButton
+            name="pen"
+            onClick={() => {
+              getLocationSrv().update({
+                partial: true,
+                query: {
+                  id: check.id,
+                },
+              });
+            }}
           />
-        </HorizontalGroup>
-      </HorizontalGroup>
+          <IconButton name="trash-alt" />
+        </ButtonGroup>
+      </div>
+      <div className={styles.stats}>
+        <UptimeGauge
+          labelNames={['instance', 'job']}
+          labelValues={[check.target, check.job]}
+          height={70}
+          width={150}
+          sparkline={false}
+        />
+      </div>
     </div>
   );
 };
