c6cdc3a8af8089e1f9ad99c3eba035e9f1715f4f
CircleCI Automation <eng@grafana.com>
2021-03-02 20:19:09 +0000

    automated release 0.8.1 [skip ci]

--- src/sessionStorage.ts
+++
@@ -1,9 +0,0 @@
-export const DASHBOARD_UPDATE_NOTIFICATION_KEY = 'hasDismissedDashboardUpdate';
-
-export function hasDismissedDashboardUpdateModal() {
-  return Boolean(window.sessionStorage.getItem(DASHBOARD_UPDATE_NOTIFICATION_KEY));
-}
-
-export function persistDashboardModalDismiss() {
-  window.sessionStorage.setItem(DASHBOARD_UPDATE_NOTIFICATION_KEY, 'true');
-}
