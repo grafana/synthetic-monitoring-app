5224d07953a72f147eb82d44c54febba07fa01be
CircleCI Automation <eng@grafana.com>
2021-03-08 23:35:54 +0000

    automated release 0.8.7 [skip ci]

--- src/datasource/module.ts
+++
@@ -1,9 +0,0 @@
-import { DataSourcePlugin } from '@grafana/data';
-import { SMDataSource } from './DataSource';
-import { ConfigEditor } from './ConfigEditor';
-import { QueryEditor } from './QueryEditor';
-import { SMQuery, SMOptions } from './types';
-
-export const plugin = new DataSourcePlugin<SMDataSource, SMQuery, SMOptions>(SMDataSource)
-  .setConfigEditor(ConfigEditor)
-  .setQueryEditor(QueryEditor);
