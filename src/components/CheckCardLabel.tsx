15b00504bbcdc512428f725a9d73dc691946a92a
Russ <8377044+rdubrock@users.noreply.github.com>
2021-03-22 17:27:48 -0800

    fix: add validation for label names and values, truncate long labels in the card view, and handle label deletion (#241)
    * fix: add length validation for label names and values, truncate long labels in the card view
    
    * fix: truncate label values in the check list view
    
    * fix: delete labels when the entire array has been deleted
    
    * refactor: pull out label length bump until changes can be made on backend

--- src/components/CheckCardLabel.tsx
+++ src/components/CheckCardLabel.tsx
@@ -2,23 +2,32 @@ import { GrafanaTheme } from '@grafana/data';
 import { Tag, useStyles } from '@grafana/ui';
 import React from 'react';
 import { Label } from 'types';
-import { css } from 'emotion';
+import { css, cx } from 'emotion';
 
 interface Props {
   label: Label;
   onLabelSelect: (label: Label) => void;
+  className?: string;
 }
 
 const getStyles = (theme: GrafanaTheme) => ({
   container: css`
     background-color: #9933cc;
     border-radius: 1px;
+    max-width: 600px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
   `,
 });
 
-export const CheckCardLabel = ({ label, onLabelSelect }: Props) => {
+export const CheckCardLabel = ({ label, onLabelSelect, className }: Props) => {
   const styles = useStyles(getStyles);
   return (
-    <Tag onClick={() => onLabelSelect(label)} name={`${label.name}: ${label.value}`} className={styles.container} />
+    <Tag
+      onClick={() => onLabelSelect(label)}
+      name={`${label.name}: ${label.value}`}
+      className={cx(styles.container, className)}
+    />
   );
 };
