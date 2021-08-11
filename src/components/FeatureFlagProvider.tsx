bf3e437ea819b3740d62e1e74ac9a5ddc619343f
CircleCI Automation <eng@grafana.com>
2021-08-11 20:51:45 +0000

    automated release 1.2.22 [skip ci]

--- src/components/FeatureFlagProvider.tsx
+++
@@ -1,8 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-import { FeatureFlagContext, getFeatureContextValues } from 'contexts/FeatureFlagContext';
-
-interface Props {}
-
-export const FeatureFlagProvider = ({ children }: PropsWithChildren<Props>) => {
-  return <FeatureFlagContext.Provider value={getFeatureContextValues()}>{children}</FeatureFlagContext.Provider>;
-};
