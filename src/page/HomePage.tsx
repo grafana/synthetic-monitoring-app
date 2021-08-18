6188040415a64565fc5cffadb5bf5438ac9cb3fe
Russ <8377044+rdubrock@users.noreply.github.com>
2021-08-18 13:57:14 -0800

    chore: stub out usage value display

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,10 +1,21 @@
-import { GrafanaTheme2, UrlQueryMap } from '@grafana/data';
-import { Button, ButtonGroup, HorizontalGroup, Icon, Link, useStyles2 } from '@grafana/ui';
+import { GrafanaTheme2 } from '@grafana/data';
+import {
+  BigValue,
+  BigValueColorMode,
+  BigValueGraphMode,
+  BigValueTextMode,
+  HorizontalGroup,
+  Icon,
+  Link,
+  LinkButton,
+  useStyles2,
+} from '@grafana/ui';
 import { DisplayCard } from 'components/DisplayCard';
 import { FeaturesBanner } from 'components/FeaturesBanner';
 import { css, cx } from '@emotion/css';
 import React from 'react';
 import { PLUGIN_URL_PATH } from 'components/constants';
+import { config } from '@grafana/runtime';
 
 const getStyles = (theme: GrafanaTheme2) => ({
   flexRow: css`
@@ -20,10 +31,15 @@ const getStyles = (theme: GrafanaTheme2) => ({
     grid-template-columns: repeat(auto-fill, minmax(300px, auto));
     grid-gap: ${theme.spacing(1)};
   `,
+  rowCard: css`
+    display: flex;
+    flex-direction: column;
+  `,
   cardGrid: css`
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(350px, auto));
     grid-gap: ${theme.spacing(2)};
+    margin-bottom: ${theme.spacing(2)};
   `,
   quickLink: css`
     background-color: ${theme.colors.background.primary};
@@ -37,6 +53,11 @@ const getStyles = (theme: GrafanaTheme2) => ({
   quickLinkIcon: css`
     color: ${theme.colors.text.link};
     margin-right: ${theme.spacing(2)};
+  `,
+  usageGrid: css`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, auto));
+    grid-gap: ${theme.spacing(1)};
   `,
   link: css`
     color: ${theme.colors.text.link};
@@ -96,18 +117,33 @@ const HomePage = () => {
           </p>
           <Link className={styles.link}>Read more about setting up checks {'>'}</Link>
           <div className={styles.actionContainer}>
-            <Button variant="secondary">Create a check</Button>
+            <LinkButton variant="secondary" href={`${PLUGIN_URL_PATH}?page=checks`}>
+              Create a check
+            </LinkButton>
           </div>
         </DisplayCard>
         <DisplayCard className={cx(styles.card, styles.rowCard)}>
           <DisplayCard.Header text="Set up checks programmatically" icon="brackets" />
           <p>Create, configure, and manage checks programmatically via Grizzly or Terraform.</p>
           <Link className={styles.link}>Learn more about creating checks programmatically {'>'}</Link>
-          {/* <div className={styles.actionContainer}> */}
           <div className={styles.actionContainer}>
             <HorizontalGroup>
-              <Button variant="secondary">Grizzly repo</Button>
-              <Button variant="secondary">Terraform repo</Button>
+              <LinkButton
+                variant="secondary"
+                target="_blank"
+                rel="noopenner noreferrer"
+                href="https://github.com/grafana/grizzly"
+              >
+                Grizzly repo
+              </LinkButton>
+              <LinkButton
+                variant="secondary"
+                href="https://registry.terraform.io/providers/grafana/grafana/latest/docs"
+                target="_blank"
+                rel="noopenner noreferrer"
+              >
+                Terraform docs
+              </LinkButton>
             </HorizontalGroup>
           </div>
           {/* </div> */}
@@ -117,12 +153,71 @@ const HomePage = () => {
           <p>Use default alerts for your checks or customize these alerts to meet your needs.</p>
           <Link className={styles.link}>Read more about synthetic monitoring alerts {'>'}</Link>
           <div className={styles.actionContainer}>
-            <Button variant="secondary">Configure alerts</Button>
+            <LinkButton variant="secondary" href={`${PLUGIN_URL_PATH}?page=alerts`}>
+              Configure alerts
+            </LinkButton>
           </div>
         </DisplayCard>
       </div>
-      <div className={styles.flexRow}>
-        <DisplayCard className={styles.card}>122 total checks</DisplayCard>
+      <div className={styles.cardGrid}>
+        <DisplayCard className={cx(styles.card, styles.usageGrid)}>
+          <BigValue
+            theme={config.theme2}
+            textMode={BigValueTextMode.ValueAndName}
+            colorMode={BigValueColorMode.Value}
+            graphMode={BigValueGraphMode.Area}
+            height={100}
+            width={150}
+            value={{
+              numeric: 122,
+              color: config.theme2.colors.text.primary,
+              title: 'Total checks',
+              text: '122',
+            }}
+          />
+          <BigValue
+            theme={config.theme2}
+            textMode={BigValueTextMode.ValueAndName}
+            colorMode={BigValueColorMode.Value}
+            graphMode={BigValueGraphMode.Area}
+            height={100}
+            width={150}
+            value={{
+              numeric: 35830,
+              color: config.theme2.colors.text.primary,
+              title: 'Total active series',
+              text: '35,830',
+            }}
+          />
+          <BigValue
+            theme={config.theme2}
+            textMode={BigValueTextMode.ValueAndName}
+            colorMode={BigValueColorMode.Value}
+            graphMode={BigValueGraphMode.Area}
+            height={100}
+            width={150}
+            value={{
+              numeric: 3608,
+              color: config.theme2.colors.text.primary,
+              title: 'Metrics',
+              text: '3,608',
+            }}
+          />
+          <BigValue
+            theme={config.theme2}
+            textMode={BigValueTextMode.ValueAndName}
+            colorMode={BigValueColorMode.Value}
+            graphMode={BigValueGraphMode.Area}
+            height={100}
+            width={125}
+            value={{
+              numeric: 4.0,
+              color: config.theme2.colors.text.primary,
+              title: 'Logs',
+              text: '4GB',
+            }}
+          />
+        </DisplayCard>
         <DisplayCard className={styles.card}></DisplayCard>
       </div>
     </div>
