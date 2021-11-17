54ba79f86877615a01ac1d6c257a46c05f7aac41
Russ <8377044+rdubrock@users.noreply.github.com>
2021-11-17 09:17:48 -0900

    feat: turn on traceroute (#380)

--- src/contexts/FeatureFlagContext.ts
+++ src/contexts/FeatureFlagContext.ts
@@ -10,6 +10,10 @@ export interface FeatureFlagContextValue {
 }
 
 export function isFeatureEnabled(name: FeatureName) {
+  // Override traceroute feature flag until we're sure we don't need it anymore
+  if (name === FeatureName.Traceroute) {
+    return true;
+  }
   const featuresParam = urlUtil.getUrlSearchParams()['features'];
   let isEnabledThroughQueryParam = false;
   if (isArray(featuresParam)) {
