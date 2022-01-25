8844ce1c666f67be89652a4b3f8d3573b5f1893c
Coleman Rollins <crollins199@gmail.com>
2022-01-25 10:51:30 -0600

    feat: [cr] adds a simple map with lat/long coordinates on probe edit page (#412)
    * feat: [cr] adds a simple map with lat/long coordinates on probe edit page
    
    * feat: [cr] load map data locally
    
    * feat: [cr] use geoJSON as a string
    
    * chore: [cr] bump version and changelog

---
+++ src/components/SimpleMap/SimpleMap.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ComposableMap, Geographies, Geography, Marker } from 'react-simple-maps';
+import geo from './geo';
+
+interface Props {
+  latitude: number;
+  longitude: number;
+}
+
+export const SimpleMap = ({ latitude, longitude }: Props) => {
+  return (
+    <div>
+      <ComposableMap>
+        <Geographies geography={JSON.parse(geo)}>
+          {({ geographies }) => geographies.map((geo) => <Geography key={geo.rsmKey} geography={geo} />)}
+        </Geographies>
+        <Marker coordinates={[Number(longitude), Number(latitude)]}>
+          <circle r={8} fill="#F53" />
+        </Marker>
+      </ComposableMap>
+    </div>
+  );
+};
