81478feec79293272ce4b1d4c4ec3774eab5fa0c
Suraj Nath <9503187+electron0zero@users.noreply.github.com>
2022-03-25 22:54:39 +0530

    fix: use increase() instead of idelta() to handle counter resets in Uptime Query

--- src/components/SuccessRateContextProvider.tsx
+++ src/components/SuccessRateContextProvider.tsx
@@ -146,8 +146,8 @@ export function SuccessRateContextProvider({ checks, probes, children }: PropsWi
       const probeReachabilityQuery =
         'sum(rate(probe_all_success_sum[3h])) by (probe) / sum(rate(probe_all_success_count[3h])) by (probe)';
 
-      const checkUptimeQuery = `sum_over_time((ceil(sum by (instance, job) (idelta(probe_all_success_sum[5m])) / sum by (instance, job) (idelta(probe_all_success_count[5m]))))[3h:]) 
-            / count_over_time((sum by (instance, job) (idelta(probe_all_success_count[5m])))[3h:])`;
+      const checkUptimeQuery = `sum_over_time((ceil(sum by (instance, job) (increase(probe_all_success_sum[5m])) / sum by (instance, job) (increase(probe_all_success_count[5m]))))[3h:])
+            / count_over_time((sum by (instance, job) (increase(probe_all_success_count[5m])))[3h:])`;
 
       const successRateType = checks ? SuccessRateTypes.Checks : SuccessRateTypes.Probes;
 
