7a398f9e5be492a9c933eee058c9c2f9dcb230a4
Suraj Nath <9503187+electron0zero@users.noreply.github.com>
2022-04-07 21:03:19 +0530

    chore: remove circle ci code and run shellcheck

--- scripts/lint-dashboards.sh
+++ scripts/lint-dashboards.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-# make sure we have lastest linter
+# make sure we have latest linter
 go install github.com/grafana/dashboard-linter@latest
 
 # check if we have it
-which dashboard-linter
+dashboard-linter -h
 
 # get all dashboards
 dashboards=$(ls ./src/dashboards/*.json)
 
 for dash in ${dashboards} ; do
   echo "Linting: ${dash}"
-  dashboard-linter lint ${dash}
+  dashboard-linter lint "${dash}"
   echo "-------------------------------------"
 done
