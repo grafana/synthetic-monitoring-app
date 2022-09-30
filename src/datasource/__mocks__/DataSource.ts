0fdd6f27f4e9d9faae297534e3102bc09a4b56cf
Russ <8377044+rdubrock@users.noreply.github.com>
2022-09-30 10:50:16 -0800

    chore: align all the grafana packages on 9.1.1

--- src/datasource/__mocks__/DataSource.ts
+++ src/datasource/__mocks__/DataSource.ts
@@ -8,7 +8,6 @@ export const instanceSettings: DataSourceInstanceSettings<SMOptions> = {
   type: 'synthetic-monitoring-datasource',
   name: 'Synthetic Monitoring',
   access: 'proxy',
-  readOnly: false,
   meta: {
     type: PluginType.datasource,
     name: 'Synthetic Monitoring API',
