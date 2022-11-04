6d6bcf21bd781348b0cf2367813f4ca6aeb00a6b
Russ <8377044+rdubrock@users.noreply.github.com>
2022-11-04 12:59:01 -0800

    chore: parse results by probe

--- src/components/CheckTestResultsModal.tsx
+++ src/components/CheckTestResultsModal.tsx
@@ -1,5 +1,5 @@
 import { dateTime } from '@grafana/data';
-import { Modal } from '@grafana/ui';
+import { Icon, Modal } from '@grafana/ui';
 import { useLogData } from 'hooks/useLogData';
 import React, { useState } from 'react';
 import { AdHocCheckResponse } from 'types';
@@ -14,28 +14,43 @@ export function CheckTestResultsModal({ testResponse, isOpen, onDismiss }: Props
   console.log('hello', testResponse?.id);
   const query = `{type="adhoc"} |= "${testResponse?.id}"`;
   const [now] = useState(Date.now());
+  const [resultsByProbe, setResultsByProbe] = useState<Record<string, any>>({});
   const start = dateTime(now).subtract(5, 'm');
   const end = dateTime(now);
   const { data } = useLogData(query, { start, end, skip: !testResponse || !isOpen });
-  // const thing =
 
-  console.log(data);
-
-  const logsStr = data?.[0]?.values?.[0]?.[1];
-  let info;
-  if (logsStr) {
+  data.forEach((item) => {
+    const logsStr = item.values?.[0]?.[1];
     try {
-      info = JSON.parse(logsStr);
+      const info = JSON.parse(logsStr);
+      if (!resultsByProbe[info.probe]) {
+        setResultsByProbe({ ...resultsByProbe, [info.probe]: info });
+      }
     } catch (e) {
       console.log('error parsing', e);
     }
-  }
-  console.log(logsStr, info);
+  });
+
+  console.log(resultsByProbe);
+
   return (
     <Modal title="Test check" isOpen={isOpen} onDismiss={onDismiss}>
       {testResponse?.target}
-      {info?.logs?.map((log: string, index: number) => {
-        return <div key={index}>log line: {log?.msg}</div>;
+      {Object.entries(resultsByProbe).map(([probe, results]) => {
+        console.log(results);
+        const successMetric = results.timeseries.find((timeseries) => timeseries.name === 'probe_success');
+        const success = successMetric?.metric?.[0]?.gauge?.value;
+
+        return (
+          <div key={probe}>
+            Probe: {probe} <Icon name={success ? 'check-circle' : 'x'} />
+            <br />
+            Lines:{' '}
+            {results.logs.map((log: any, index: number) => {
+              return <div key={index}>{log.msg}</div>;
+            })}
+          </div>
+        );
       })}
     </Modal>
   );
