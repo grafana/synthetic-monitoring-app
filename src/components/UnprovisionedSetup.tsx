3450575de247833a9b194ce4f1e8acfac24ae38d
Russ <8377044+rdubrock@users.noreply.github.com>
2023-01-06 08:45:44 -0900

    chore: max a max width for the error container

--- src/components/UnprovisionedSetup.tsx
+++ src/components/UnprovisionedSetup.tsx
@@ -5,6 +5,9 @@ import { GrafanaTheme2 } from '@grafana/data';
 import { css } from '@emotion/css';
 
 const getStyles = (theme: GrafanaTheme2) => ({
+  container: css`
+    max-width: 1000px;
+  `,
   link: css`
     text-decoration: underline;
   `,
@@ -15,25 +18,27 @@ export const UnprovisionedSetup = () => {
 
   return (
     <PluginPage pageNav={{ text: 'Invalid provisioning', description: 'Provisioning missing or invalid' }}>
-      <Alert title="Provisioning missing or invalid" severity="error">
-        Provisioning is required for Synthetic Monitoring
-      </Alert>
-      <p>
-        It looks like your Grafana instance hasn&apos;t been provisioned correctly to use the Synthetic Monitoring
-        plugin.
-      </p>
-      <ul>
-        <li>
-          <a className={styles.link} href="https://grafana.com/docs/grafana/latest/administration/provisioning/">
-            Learn more about what provisioning is
-          </a>
-        </li>
-        <li>
-          <a className={styles.link} href="https://github.com/grafana/synthetic-monitoring-app/#configuration">
-            Learn how to provision Synthetic Monitoring
-          </a>
-        </li>
-      </ul>
+      <div className={styles.container}>
+        <Alert title="Provisioning missing or invalid" severity="error">
+          Provisioning is required for Synthetic Monitoring
+        </Alert>
+        <p>
+          It looks like your Grafana instance hasn&apos;t been provisioned correctly to use the Synthetic Monitoring
+          plugin.
+        </p>
+        <ul>
+          <li>
+            <a className={styles.link} href="https://grafana.com/docs/grafana/latest/administration/provisioning/">
+              Learn more about what provisioning is
+            </a>
+          </li>
+          <li>
+            <a className={styles.link} href="https://github.com/grafana/synthetic-monitoring-app/#configuration">
+              Learn how to provision Synthetic Monitoring
+            </a>
+          </li>
+        </ul>
+      </div>
     </PluginPage>
   );
 };
