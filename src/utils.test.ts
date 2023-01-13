c2451733cd787c62e18be6afe3a0c4f09f465d35
Russ <8377044+rdubrock@users.noreply.github.com>
2023-01-13 11:04:12 -0900

    fix: check test runs should be limited to 5 probes (#509)
    * fix: check test runs should be limited to 5 probes
    
    * chore: sort by number value

---
+++ src/utils.test.ts
@@ -0,0 +1,15 @@
+import { getRandomProbes } from 'utils';
+
+it('gets random probes', () => {
+  const probes = [11, 23, 5, 5212, 43, 3, 4, 6];
+  const random = getRandomProbes(probes, 4);
+  expect(random.length).toBe(4);
+
+  random.forEach((randomId) => {
+    const found = probes.findIndex((probeId) => probeId === randomId);
+    expect(found).toBeGreaterThan(-1);
+  });
+
+  const random2 = getRandomProbes(probes, 2);
+  expect(random2.length).toBe(2);
+});
