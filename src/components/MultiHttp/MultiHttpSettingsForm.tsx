510e7d6f5dfb3db07f39109875d4330ed2d34549
Cat Perry <000.perry@gmail.com>
2023-01-18 08:53:03 -0800

    feat: all but on Select working

--- src/components/MultiHttp/MultiHttpSettingsForm.tsx
+++ src/components/MultiHttp/MultiHttpSettingsForm.tsx
@@ -4,7 +4,9 @@ import { FormProvider, useForm, Controller, useFieldArray, useFormContext } from
 import { useParams } from 'react-router-dom';
 
 import {
+  Alert,
   Button,
+  Collapse,
   Field,
   VerticalGroup,
   Input,
@@ -17,11 +19,10 @@ import {
 } from '@grafana/ui';
 import { GrafanaTheme2 } from '@grafana/data';
 import { getDefaultValuesFromCheck, selectableValueFrom } from 'components/CheckEditor/checkFormTransformations';
-import { Collapse } from 'components/Collapse';
 import { ProbeOptions } from 'components/CheckEditor/ProbeOptions';
-import { LabelField } from 'components/LabelField';
 import { methodOptions } from 'components/constants';
 import { multiHttpFallbackCheck } from './consts';
+import { Subheader } from 'components/Subheader';
 import { validateTarget } from 'validation';
 import CheckTarget from 'components/CheckTarget';
 import { RequestTabs } from './Tabs/Tabs';
@@ -37,9 +38,11 @@ interface Props {
 }
 
 export const MultiHttpSettingsForm = ({ isEditor, checks, onReturn }: Props) => {
-  const [showAdvanced, setShowAdvanced] = useState(false);
   const [activeTab, setActiveTab] = useState('header');
   const styles = useStyles2(getStyles);
+  const [showRequest, setShowRequest] = useState(true);
+  const [urls, setUrls] = useState<any[]>([]);
+  const [errorMessages, setErrorMessages] = useState([]);
 
   const {
     register,
@@ -68,61 +71,101 @@ export const MultiHttpSettingsForm = ({ isEditor, checks, onReturn }: Props) => 
 
   const onSubmit = useCallback(
     async (evt: any, data: any) => {
-      const updatedCheck = {
-        alertSensitivity: getValues().alertSensitivity.value,
-        basicMetricsOnly: getValues().basicMetricsOnly,
-        enabled: getValues().enabled,
-        frequency: 120000,
-        job: getValues().job,
-        settings: { multihttp: getValues().settings.multihttp },
-        labels: getValues().labels,
-        probes: [1],
-        target: getValues().settings.multihttp.entries[0].request.url,
-        timeout: 3000,
-      };
+      try {
+        const updatedCheck = {
+          alertSensitivity: getValues().alertSensitivity.value,
+          basicMetricsOnly: getValues().basicMetricsOnly,
+          enabled: getValues().enabled,
+          frequency: 120000,
+          job: getValues().job,
+          settings: { multihttp: getValues().settings.multihttp },
+          labels: getValues().labels,
+          probes: [1],
+          target: getValues().settings.multihttp.entries[0].request.url,
+          timeout: 3000,
+        };
 
-      if (check?.id) {
-        trackEvent('editCheckSubmit');
-        await api?.updateCheck({
-          id: check.id,
-          tenantId: check.tenantId,
-          ...updatedCheck,
-        });
-      } else {
-        trackEvent('addNewCheckSubmit');
-        await api?.addCheck(updatedCheck);
+        if (check?.id) {
+          trackEvent('editCheckSubmit');
+          await api?.updateCheck({
+            id: check.id,
+            tenantId: check.tenantId,
+            ...updatedCheck,
+          });
+        } else {
+          trackEvent('addNewCheckSubmit');
+          await api?.addCheck(updatedCheck);
+        }
+        onReturn && onReturn(true);
+      } catch (err) {
+        setErrorMessages([err?.data?.err || err?.data?.msg]);
       }
-      onReturn && onReturn(true);
     },
     [api, getValues, onReturn, check.tenantId, check.id]
   );
 
+  const clearAlert = () => {
+    setErrorMessages([]);
+  };
+
+  const getErrorMessages = () => {
+    return (
+      <Alert title="DS creation failed" severity="error" onRemove={clearAlert}>
+        <>
+          {errorMessages.map((ms, index) => {
+            return <div key={index}>{ms}</div>;
+          })}
+        </>
+      </Alert>
+    );
+  };
+
   return (
     <>
+      {errorMessages.length > 0 && getErrorMessages()}
+
       <hr className={styles.breakLine} />
-      <Field label="Check name" disabled={!isEditor} required invalid={Boolean(errors.job)} error={errors.job?.message}>
+      <Subheader>Check name</Subheader>
+      <Field disabled={!isEditor} invalid={Boolean(errors.job)} error={errors.job?.message}>
         <Input
           {...register('job', {
             required: true,
           })}
           type="text"
           placeholder="Unnamed request"
+          className={styles.jobNameInput}
         />
       </Field>
+      <ProbeOptions
+        {...register('probes' as const, {
+          required: true,
+        })}
+        isEditor={isEditor}
+        timeout={check?.timeout ?? multiHttpFallbackCheck.timeout}
+        frequency={check?.frequency ?? multiHttpFallbackCheck.frequency}
+        probes={[1]} // TODO: FIX THIS
+      />
 
       <hr />
-      <h4>Requests</h4>
+      <h3>Requests</h3>
       <Field label="At least one target HTTP is required. Let's get started.">
         <></>
       </Field>
       <div className={styles.request}>
         <VerticalGroup>
           <FormProvider {...formMethods}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              {fields.map(({ id }, index) => {
+            <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+              {fields.map((field, index) => {
                 return (
-                  <div key={index}>
-                    <VerticalGroup>
+                  <Collapse
+                    label={urls.length > 0 && Object.values(urls[index] || {})}
+                    key={field.id}
+                    onToggle={() => setShowRequest(!showRequest)}
+                    isOpen={showRequest}
+                    collapsible
+                    className={styles.collapseTarget}
+                  >
+                    <VerticalGroup height={'100%'}>
                       <HorizontalGroup spacing="lg" align="center">
                         <Controller
                           {...register(`settings.multihttp.entries[${index}].request.url`, {
@@ -142,11 +185,19 @@ export const MultiHttpSettingsForm = ({ isEditor, checks, onReturn }: Props) => 
                             return (
                               <CheckTarget
                                 {...field}
-                                onBlur={field.onBlur}
+                                value={field.value || Object.values(urls[index] || {})}
                                 typeOfCheck={selectedCheckType}
                                 invalid={Boolean(errors.url)}
                                 error={errors.url?.message}
                                 disabled={!isEditor}
+                                onBlur={(url) => {
+                                  if (!urls.find((u, i) => i === index)) {
+                                    setUrls([...urls, { [index]: url.target.value }]);
+                                  } else {
+                                    const filteredChecks = urls.filter((u, i) => i !== index);
+                                    setUrls([...filteredChecks, { [index]: url.target.value }]);
+                                  }
+                                }}
                               />
                             );
                           }}
@@ -154,33 +205,32 @@ export const MultiHttpSettingsForm = ({ isEditor, checks, onReturn }: Props) => 
                         <Field
                           label="Request method"
                           description="The HTTP method used"
-                          // disabled={!isEditor}
+                          disabled={!isEditor}
                           invalid={Boolean(errors?.settings?.http?.method)}
                           error={errors?.settings?.http?.method}
                         >
                           <Controller
-                            {...register(`settings.multihttp.entries[${index}].request.method`, {
-                              required: true,
-                            })}
                             control={control}
-                            render={({ field }) => {
-                              console.log('field', field);
-
+                            render={({ field: { onChange, value } }) => {
                               return (
                                 <Select
                                   {...field}
                                   options={methodOptions}
-                                  onChange={(val) => selectableValueFrom(val)}
+                                  onChange={(val) => onChange(val.value)}
+                                  value={value}
                                 />
                               );
                             }}
                             rules={{ required: true }}
-                            defaultValue={methodOptions.find((val) => val.value === 'GET')?.value}
+                            name={`settings.multihttp.entries[${index}].request.method`}
                           />
                         </Field>
                         <Button
                           variant="secondary"
-                          onClick={() => remove(index)}
+                          onClick={() => {
+                            setUrls(urls.filter((url, i) => i !== index));
+                            remove(index);
+                          }}
                           className={styles.removeRequestButton}
                         >
                           Remove
@@ -193,12 +243,19 @@ export const MultiHttpSettingsForm = ({ isEditor, checks, onReturn }: Props) => 
                           active={activeTab === 'header'}
                           onChangeTab={() => setActiveTab('header')}
                           default={true}
+                          className={styles.tabs}
                         />
-                        <Tab label={'Body'} active={activeTab === 'body'} onChangeTab={() => setActiveTab('body')} />
+                        <Tab
+                          className={styles.tabs}
+                          label={'Body'}
+                          active={activeTab === 'body'}
+                          onChangeTab={() => setActiveTab('body')}
+                        />
                         <Tab
                           label={'Query Params'}
                           active={activeTab === 'queryParams'}
                           onChangeTab={() => setActiveTab('queryParams')}
+                          className={styles.tabs}
                         />
                       </TabsBar>
                       <TabContent className={styles.tabsContent}>
@@ -213,51 +270,34 @@ export const MultiHttpSettingsForm = ({ isEditor, checks, onReturn }: Props) => 
                         />
                       </TabContent>
                     </VerticalGroup>
-                  </div>
+                  </Collapse>
                 );
               })}
-              <Button
-                type="button"
-                fill="text"
-                size="lg"
-                icon="plus"
-                onClick={() => append({})}
-                className={styles.addRequestButton}
-              >
-                Add a request to Multi-HTTP check
-              </Button>
-              <ProbeOptions
-                {...register('probes' as const, {
-                  required: true,
-                })}
-                isEditor={isEditor}
-                timeout={check?.timeout ?? multiHttpFallbackCheck.timeout}
-                frequency={check?.frequency ?? multiHttpFallbackCheck.frequency}
-                probes={[1]}
-              />
-
-              <Button type="button" onClick={(evt, data) => onSubmit(evt, data)}>
-                Submit
-              </Button>
+              <VerticalGroup>
+                <Button
+                  type="button"
+                  fill="text"
+                  size="md"
+                  icon="plus"
+                  onClick={() => append({})}
+                  className={styles.addRequestButton}
+                >
+                  Add request
+                </Button>
+                <Button
+                  type="button"
+                  onClick={(evt, data) => onSubmit(evt, data)}
+                  fullWidth={true}
+                  className={styles.submitMultiHttpButton}
+                  size="lg"
+                >
+                  Submit
+                </Button>
+              </VerticalGroup>
             </form>
           </FormProvider>
         </VerticalGroup>
       </div>
-
-      <Collapse
-        label="Advanced options"
-        collapsible={true}
-        onToggle={() => setShowAdvanced(!showAdvanced)}
-        isOpen={showAdvanced}
-      >
-        <div
-          className={css`
-            max-width: 500px;
-          `}
-        >
-          <LabelField isEditor={isEditor} />
-        </div>
-      </Collapse>
     </>
   );
 };
@@ -270,6 +310,16 @@ const getStyles = (theme: GrafanaTheme2) => ({
     justify-content: space-evenly;
     gap: 20px;
     align-self: flex-start;
+    align-items: content;
+    position: relative;
+  `,
+  collapseTarget: css`
+    width: 90vw;
+    background-color: ${theme.colors.background.secondary};
+    padding: 10px;
+  `,
+  jobNameInput: css`
+    width: 100%;
   `,
   reqMethod: css`
     align-self: flex-start;
@@ -283,9 +333,16 @@ const getStyles = (theme: GrafanaTheme2) => ({
   tabsContent: css`
     min-height: 75px;
     margin-bottom: 15px;
+    width: 100%;
   `,
   tabsBar: css`
     margin-top: -10px;
+    width: 100%;
+
+    gap: 30px;
+  `,
+  tabs: css`
+    min-width: 150px;
   `,
   addRequestButton: css`
     margin-bottom: 16px;
@@ -293,5 +350,11 @@ const getStyles = (theme: GrafanaTheme2) => ({
   removeRequestButton: css`
     align-self: auto;
     margin-top: 20px;
+  `,
+  form: css`
+    position: relative;
+  `,
+  submitMultiHttpButton: css`
+    width: 100%;
   `,
 });
