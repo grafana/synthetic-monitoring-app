9ff00b4eb4b962a3c13f4dd9b3d8a761e8923d28
Russ <8377044+rdubrock@users.noreply.github.com>
2023-02-09 10:52:43 -0900

    chore: display available variables

--- src/components/MultiHttp/AvailableVariables.tsx
+++ src/components/MultiHttp/AvailableVariables.tsx
@@ -1,14 +1,25 @@
-import { Card, HorizontalGroup } from '@grafana/ui';
+import { GrafanaTheme2 } from '@grafana/data';
+import { ClipboardButton, Field, HorizontalGroup, useStyles2 } from '@grafana/ui';
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
-import { CheckFormValues, MultiHttpSettings, MultiHttpVariable, SettingsFormValues } from 'types';
-import { MultiHttpEntry, VariableType } from './MultiHttpTypes';
+import { SettingsFormValues } from 'types';
+import { VariableType } from './MultiHttpTypes';
+import { css } from '@emotion/css';
 
 interface Props {
   index: number;
 }
 
+function getStyles(theme: GrafanaTheme2) {
+  return {
+    container: css`
+      margin-top: ${theme.spacing(1)};
+    `,
+  };
+}
+
 export function AvailableVariables({ index }: Props) {
+  const styles = useStyles2(getStyles);
   const { watch } = useFormContext();
   const settings = (watch('settings') as SettingsFormValues) ?? {};
   const availableVars =
@@ -24,14 +35,20 @@ export function AvailableVariables({ index }: Props) {
   }
 
   return (
-    <div>
-      {availableVars.map((variable, i) => {
-        return (
-          <HorizontalGroup key={i}>
-            <code>{`\$\{${variable.name}\}`}</code>
-          </HorizontalGroup>
-        );
-      })}
-    </div>
+    <Field label="Available variables">
+      <div className={styles.container}>
+        {availableVars.map((variable, i) => {
+          return (
+            <ClipboardButton
+              key={i}
+              getText={() => '${' + variable.name + '}'}
+              variant="secondary"
+              fill="text"
+              icon="copy"
+            >{`\$\{${variable.name}\}`}</ClipboardButton>
+          );
+        })}
+      </div>
+    </Field>
   );
 }
