a74f379544f58770f5ea1fb8e387f13be531625e
Russ <8377044+rdubrock@users.noreply.github.com>
2023-02-09 09:31:03 -0900

    chore: stub out variable names

--- src/components/MultiHttp/Tabs/TabSection.tsx
+++ src/components/MultiHttp/Tabs/TabSection.tsx
@@ -3,19 +3,20 @@ import { useStyles2, TabsBar, TabContent, Tab } from '@grafana/ui';
 import { RequestTabs } from 'components/MultiHttp/Tabs/Tabs';
 import { getMultiHttpFormStyles } from './../MultiHttpSettingsForm.styles';
 import { useFormContext } from 'react-hook-form';
+import { MultiHttpFormTabs } from 'types';
 
-type ActiveTabTypes = 'header' | 'queryParams' | 'body';
 interface RequestTabsProps {
   label?: string;
   index: number;
 }
 
 export const TabSection = ({ index }: RequestTabsProps) => {
-  const [activeTab, setActiveTab] = useState<ActiveTabTypes>('header');
+  const [activeTab, setActiveTab] = useState<MultiHttpFormTabs>('header');
   const styles = useStyles2(getMultiHttpFormStyles);
 
   const { formState } = useFormContext();
 
+  console.log({ activeTab });
   return (
     <div className={styles.tabsContent}>
       <TabsBar className={styles.tabsBar}>
@@ -55,6 +56,18 @@ export const TabSection = ({ index }: RequestTabsProps) => {
             }
           }}
           className={styles.tabs}
+        />
+        <Tab
+          label="Variables"
+          className={styles.tabs}
+          active={activeTab === 'variables'}
+          onChangeTab={() => {
+            if (formState.errors?.settings?.multihttp?.entries[index]?.request) {
+              return;
+            } else {
+              setActiveTab('variables');
+            }
+          }}
         />
       </TabsBar>
       <TabContent className={styles.tabsContent}>
