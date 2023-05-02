69491a93e27a32b8999e269ce83f39c9cab304b4
Russ <8377044+rdubrock@users.noreply.github.com>
2023-05-02 11:37:07 -0800

    chore: add new check routing

---
+++ src/components/NewScriptedCheck/NewScriptedCheck.tsx
@@ -0,0 +1,42 @@
+import { PluginPage } from '@grafana/runtime';
+import { HorizontalGroup, Card, VerticalGroup, LinkButton } from '@grafana/ui';
+import React from 'react';
+import { ScriptEditorIllustration } from './ScriptEditorIllustration';
+import { TestBuilderIllustration } from './TestBuilderIllustration';
+import { useRouteMatch } from 'react-router-dom';
+
+export function NewScriptedCheck() {
+  const { path } = useRouteMatch();
+  return (
+    <PluginPage pageNav={{ text: 'New check', description: 'Choose creation method' }}>
+      <HorizontalGroup justify="center" wrap={true}>
+        <HorizontalGroup>
+          <Card>
+            <HorizontalGroup>
+              <VerticalGroup>
+                <h3>Test Builder</h3>
+                <p>Use our interactive UI to compose GET, POST, PUT, PATCH, DELETE, HEAD and OPTIONS requests.</p>
+                <LinkButton href={`${path}/builder`}>Start Building</LinkButton>
+              </VerticalGroup>
+              <HorizontalGroup>
+                <TestBuilderIllustration />
+              </HorizontalGroup>
+            </HorizontalGroup>
+          </Card>
+          <Card>
+            <HorizontalGroup>
+              <VerticalGroup>
+                <h3>Script Editor</h3>
+                <p>Use our code samples as a foundation for your script or start from a clean slate.</p>
+                <LinkButton href={`${path}/text-editor`}>Start Scripting</LinkButton>
+              </VerticalGroup>
+              <HorizontalGroup>
+                <ScriptEditorIllustration />
+              </HorizontalGroup>
+            </HorizontalGroup>
+          </Card>
+        </HorizontalGroup>
+      </HorizontalGroup>
+    </PluginPage>
+  );
+}
--- src/page/ScriptedChecksPage.tsx
+++ src/page/ScriptedChecksPage.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
-import { PluginPage } from '@grafana/runtime';
 import { SyntheticsBuilder } from '@grafana/k6-test-builder';
-import { useTheme2 } from '@grafana/ui';
+import { PluginPage } from '@grafana/runtime';
+import { Button, useTheme2 } from '@grafana/ui';
+import { NewScriptedCheck } from 'components/NewScriptedCheck';
+import { useNavigation } from 'hooks/useNavigation';
+import React from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import { ROUTES } from 'types';
 
 export function ScriptedChecksPage() {
   const theme = useTheme2();
   const handleSubmit = (values: any, errors: any) => {
     console.log({ values, errors });
   };
+  const navigate = useNavigation();
+  const { path } = useRouteMatch();
+
   return (
-    <PluginPage pageNav={{ text: 'Scripted checks', description: 'List of checks' }}>
-      <SyntheticsBuilder theme={theme} onSubmit={handleSubmit} />
-    </PluginPage>
+    <Switch>
+      <Route path={`${path}/new`} exact>
+        <NewScriptedCheck />
+      </Route>
+      <Route path={`${path}/new/builder`}>
+        <SyntheticsBuilder theme={theme} onSubmit={handleSubmit} />
+      </Route>
+      <Route path={`${path}/new/text-editor`}>
+        <h1>text editor</h1>
+      </Route>
+      <Route path={path}>
+        <PluginPage pageNav={{ text: 'Scripted checks', description: 'List of checks' }}>
+          <Button onClick={() => navigate(`${ROUTES.ScriptedChecks}/new`)}>Add new</Button>
+          <ul>
+            <li>check one</li>
+            <li>check two</li>
+          </ul>
+        </PluginPage>
+      </Route>
+    </Switch>
   );
 }
