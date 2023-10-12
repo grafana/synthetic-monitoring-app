e10c64c723def0463de83febedc3f077afee6b78
Chris Bedwell <christopher.bedwell@grafana.com>
2023-10-12 11:55:53 +0100

    fix: reconcile eslint files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,9 @@
 module.exports = {
-  extends: [
-    '@grafana/eslint-config',
-    'plugin:react-hooks/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-  ],
+  extends: ['./.config/.eslintrc', 'plugin:import/errors', 'plugin:import/warnings', 'plugin:import/typescript'],
   rules: {
     // Since TS shows errors for unresolved imports anyway,
     // we can safely turn this off. (Plus, eslint doesn't play
     // nice with TS' baseUrl out of the box.)
     'import/no-unresolved': 'off',
-    'react-hooks/exhaustive-deps': 'error',
   },
 };
