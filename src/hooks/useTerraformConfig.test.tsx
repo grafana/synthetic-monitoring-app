180900ff103ea20ad90fa66dd57993d08502d450
Russ <8377044+rdubrock@users.noreply.github.com>
2023-10-19 15:17:24 -0800

    chore: add a smoke test for multihttp config generation

--- src/hooks/useTerraformConfig.test.tsx
+++ src/hooks/useTerraformConfig.test.tsx
@@ -193,4 +193,159 @@ describe('terraform config generation', () => {
       },
     });
   });
+
+  test('handles multihttp checks', async () => {
+    const wrapper = ({ children }: PropsWithChildren<{}>) => {
+      const api = getInstanceMock();
+      api.listChecks = jest.fn().mockImplementation(() =>
+        Promise.resolve([
+          {
+            job: 'stuff',
+            target: 'https://www.grafana-dev.com',
+            enabled: true,
+            labels: [],
+            probes: [57],
+            timeout: 17000,
+            frequency: 120000,
+            alertSensitivity: 'none',
+            settings: {
+              multihttp: {
+                entries: [
+                  {
+                    request: { method: 'GET', url: 'https://www.grafana-dev.com', headers: [], queryFields: [{}] },
+                    variables: [],
+                    checks: [{ type: 0, subject: 2, condition: 2, value: '200' }],
+                  },
+                  {
+                    request: { url: 'https://secondrequest.com', method: 'POST', headers: [], queryFields: [] },
+                    variables: [{ type: 0, name: 'avariable', expression: 'great.variable.path' }],
+                    checks: [],
+                  },
+                  {
+                    request: { url: '${avariable}', method: 'GET', headers: [], queryFields: [] },
+                    variables: [],
+                    checks: [],
+                  },
+                ],
+              },
+            },
+            basicMetricsOnly: true,
+          },
+        ])
+      );
+      const instance = {
+        api,
+      };
+      const meta = {} as AppPluginMeta<GlobalSettings>;
+
+      return <InstanceContext.Provider value={{ instance, loading: false, meta }}>{children}</InstanceContext.Provider>;
+    };
+    const { result } = renderHook(() => useTerraformConfig(), { wrapper });
+    await waitFor(() => {
+      expect(result.current.config).not.toBeUndefined();
+    });
+    console.log(result.current.config);
+    expect(result.current.config).toEqual({
+      provider: {
+        grafana: {
+          auth: '<add an api key from grafana.com>',
+          sm_access_token: '<add an sm access token>',
+          sm_url: 'http://localhost:4030',
+          url: '',
+        },
+      },
+      resource: {
+        grafana_synthetic_monitoring_check: {
+          'stuff_https___www_grafana-dev_com': {
+            enabled: true,
+            job: 'stuff',
+            labels: {},
+            probes: [57],
+            settings: {
+              multihttp: {
+                entries: [
+                  {
+                    checks: [
+                      {
+                        condition: 2,
+                        subject: 2,
+                        type: 0,
+                        value: '200',
+                      },
+                    ],
+                    request: {
+                      body: {
+                        content_type: undefined,
+                      },
+                      headers: [],
+                      method: 'GET',
+                      queryFields: [{}],
+                      query_fields: [{}],
+                      url: 'https://www.grafana-dev.com',
+                    },
+                    variables: [],
+                  },
+                  {
+                    checks: [],
+                    request: {
+                      body: {
+                        content_type: undefined,
+                      },
+                      headers: [],
+                      method: 'POST',
+                      queryFields: [],
+                      query_fields: [],
+                      url: 'https://secondrequest.com',
+                    },
+                    variables: [
+                      {
+                        expression: 'great.variable.path',
+                        name: 'avariable',
+                        type: 0,
+                      },
+                    ],
+                  },
+                  {
+                    checks: [],
+                    request: {
+                      body: {
+                        content_type: undefined,
+                      },
+                      headers: [],
+                      method: 'GET',
+                      queryFields: [],
+                      query_fields: [],
+                      // We need double dollar signs here because of terraform interpolation
+                      url: '$${avariable}',
+                    },
+                    variables: [],
+                  },
+                ],
+              },
+            },
+            target: 'https://www.grafana-dev.com',
+          },
+        },
+        grafana_synthetic_monitoring_probe: {
+          tacos: {
+            labels: {
+              Mr: 'Orange',
+            },
+            latitude: 0,
+            longitude: 0,
+            name: 'tacos',
+            public: false,
+            region: 'EMEA',
+          },
+        },
+      },
+      terraform: {
+        required_providers: {
+          grafana: {
+            source: 'grafana/grafana',
+          },
+        },
+      },
+    });
+  });
 });
