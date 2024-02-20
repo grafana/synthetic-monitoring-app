04ded96ab2baa4ac5eed1f66707d3ad01bd246ac
Russ <8377044+rdubrock@users.noreply.github.com>
2024-02-20 13:38:22 -0900

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -14,12 +14,14 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|md)$':
       '<rootDir>/src/test/fileMock.js',
     '^!raw-loader!*': '<rootDir>/src/test/rawLoaderMock.js',
+    '^(.+)\\?raw$': '<rootDir>/src/test/rawLoaderMock.js',
   },
   // testTimeout: 30000,
   // Inform jest to only transform specific node_module packages.
   transform: {
     ...config.transform,
     '^.+\\.mjs$': ['@swc/jest'],
+    'assets/snippets/.+\\.js$': '<rootDir>/src/test/rawLoaderMock.js',
   },
   transformIgnorePatterns: [
     nodeModulesToTransform([
