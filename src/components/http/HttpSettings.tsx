4e51ccc7645456520f23b207c2202861bc27ec79
Russ <8377044+rdubrock@users.noreply.github.com>
2024-02-26 08:02:07 -0900

    Allow unlimited headers (#697)
    * fix: allow unlimited HTTP headers
    
    * chore: update types
    
    * chore: allow unlimited proxy connect headers
    
    * chore: update types

--- src/components/http/HttpSettings.tsx
+++ src/components/http/HttpSettings.tsx
@@ -211,7 +211,6 @@ export const HttpSettingsForm = ({ isEditor }: Props) => {
               name="settings.http.headers"
               disabled={!isEditor}
               label="header"
-              limit={10}
               validateName={validateHTTPHeaderName}
               validateValue={validateHTTPHeaderValue}
             />
@@ -240,7 +239,6 @@ export const HttpSettingsForm = ({ isEditor }: Props) => {
               name="settings.http.proxyConnectHeaders"
               disabled={!isEditor}
               label="proxy connect header"
-              limit={10}
               validateName={validateHTTPHeaderName}
               validateValue={validateHTTPHeaderValue}
             />
