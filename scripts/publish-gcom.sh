d75f89c977961d06e7f9e1d15b1d796987e925b0
Russ <8377044+rdubrock@users.noreply.github.com>
2024-04-11 14:18:11 -0800

    chore: move gcom publish to its own script, actually working now

---
+++ scripts/publish-gcom.sh
@@ -0,0 +1,13 @@
+ #!/bin/bash
+# This is used in Drone to generate the version string for automatic PR creation
+set -eufo pipefail
+
+VERSION="${DRONE_TAG#v}"
+
+echo "${VERSION}"
+
+curl -f -w "status=%{http_code}" -s -H "Authorization: Bearer ${GRAFANA_API_KEY}" \
+-d "download[any][url]=https://storage.googleapis.com/synthetic-monitoring-app-prod/builds/${VERSION}/grafana-synthetic-monitoring-app-${VERSION}.zip" \
+-d "download[any][md5]=$$(curl -sL https://storage.googleapis.com/synthetic-monitoring-app-prod/builds/${VERSION}/grafana-synthetic-monitoring-app-${VERSION}.zip | md5sum | cut -d' ' -f1)" \
+-d url=https://github.com/grafana/synthetic-monitoring-app \
+"https://grafana.com/api/plugins"
