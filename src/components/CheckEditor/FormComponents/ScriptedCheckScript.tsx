fbc6fa30aba454abbbccbe185a2fe5bd1568e0d5
Russ <8377044+rdubrock@users.noreply.github.com>
2024-04-24 15:19:55 -0800

    chore: make scripted examples a tab next to the editor

--- src/components/CheckEditor/FormComponents/ScriptedCheckScript.tsx
+++ src/components/CheckEditor/FormComponents/ScriptedCheckScript.tsx
@@ -1,24 +1,60 @@
 import React from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
+import { Tab, TabContent, TabsBar } from '@grafana/ui';
 
 import { CheckFormValuesScripted } from 'types';
 import { CodeEditor } from 'components/CodeEditor';
-import { ScriptExamplesMenu } from 'components/ScriptExamplesMenu/ScriptExamplesMenu';
+import { CodeSnippet } from 'components/CodeSnippet';
+import { SCRIPT_EXAMPLES } from 'components/WelcomeTabs/constants';
+
+enum ScriptEditorTabs {
+  Script = 'script',
+  Examples = 'examples',
+}
 
 export const ScriptedCheckScript = () => {
-  const { control, setValue, getValues } = useFormContext<CheckFormValuesScripted>();
-  const id = getValues('id');
+  const { control } = useFormContext<CheckFormValuesScripted>();
+  const [selectedTab, setSelectedTab] = React.useState(ScriptEditorTabs.Script);
 
   return (
     <>
-      {!id && <ScriptExamplesMenu onSelectExample={(script) => setValue('settings.scripted.script', script)} />}
-      <Controller
-        name="settings.scripted.script"
-        control={control}
-        render={({ field }) => {
-          return <CodeEditor {...field} />;
-        }}
-      />
+      {/* {!id && <ScriptExamplesMenu onSelectExample={(script) => setValue('settings.scripted.script', script)} />} */}
+      <TabsBar>
+        <Tab label="Script" onChangeTab={() => setSelectedTab(ScriptEditorTabs.Script)} />
+        <Tab label="Examples" onChangeTab={() => setSelectedTab(ScriptEditorTabs.Examples)} />
+      </TabsBar>
+      <TabContent>
+        {selectedTab === ScriptEditorTabs.Script && (
+          <Controller
+            name="settings.scripted.script"
+            control={control}
+            render={({ field }) => {
+              return <CodeEditor {...field} />;
+            }}
+          />
+        )}
+        {selectedTab === ScriptEditorTabs.Examples && (
+          <CodeSnippet
+            hideHeader
+            canCopy={true}
+            // className={styles.codeSnippet}
+            tabs={[
+              {
+                value: 'Example scripts',
+                label: '',
+                groups: SCRIPT_EXAMPLES.map(({ label, script }) => ({
+                  value: label,
+                  label,
+                  code: script,
+                  lang: 'js',
+                })),
+              },
+            ]}
+            dedent={true}
+            lang="js"
+          />
+        )}
+      </TabContent>
     </>
   );
 };
