caa4eeee67c86d458ba1acdcfdf90168aa71d13d
Heitor Tashiro Sergent <heitortsergent@gmail.com>
2024-04-24 17:17:43 -0500

    chore: k6 lowercase, make descriptions consistent

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -121,7 +121,7 @@ export const CHECK_TYPE_OPTIONS = [
   {
     label: 'MULTIHTTP',
     value: CheckType.MULTI_HTTP,
-    description: 'Check multiple web endpoints in sequence',
+    description: 'Check multiple web endpoints in sequence.',
     status: CheckStatus.PUBLIC_PREVIEW,
   },
   {
@@ -132,22 +132,22 @@ export const CHECK_TYPE_OPTIONS = [
   {
     label: 'DNS',
     value: CheckType.DNS,
-    description: 'Ensures a domain resolves and measures the average time for the resolution to happen',
+    description: 'Ensures a domain resolves and measures the average time for the resolution to happen.',
   },
   {
     label: 'TCP',
     value: CheckType.TCP,
-    description: 'Ensures a hostname and port accept a connection and measures performance',
+    description: 'Ensures a hostname and port accept a connection and measures performance.',
   },
   {
     label: 'Traceroute',
     value: CheckType.Traceroute,
-    description: 'Trace the path of a request through the internet',
+    description: 'Trace the path of a request through the internet.',
   },
   {
     label: 'Scripted',
     value: CheckType.Scripted,
-    description: 'Write a K6 script to run custom checks',
+    description: 'Write a k6 script to run custom checks.',
     status: CheckStatus.EXPERIMENTAL,
     featureToggle: FeatureName.ScriptedChecks,
   },
