59ea057f4eea439bc595af6343e6c6cfc0bffc57
Chris Bedwell <christopher.bedwell@grafana.com>
2024-05-01 15:19:00 +0100

    fix: add queryClientProvider to ConfigPageWrapper (#778)

--- src/components/ConfigPageWrapper.tsx
+++ src/components/ConfigPageWrapper.tsx
@@ -1,24 +1,24 @@
-import React, { PureComponent } from 'react';
-import { AppPluginMeta,PluginConfigPageProps } from '@grafana/data';
+import React from 'react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { AppPluginMeta, PluginConfigPageProps } from '@grafana/data';
 
 import { GlobalSettings } from 'types';
+import { queryClient } from 'data/queryClient';
 import { InstanceProvider } from 'components/InstanceProvider';
 import { ConfigPage } from 'page/ConfigPage';
 
 interface Props extends PluginConfigPageProps<AppPluginMeta<GlobalSettings>> {}
 
-export class ConfigPageWrapper extends PureComponent<Props> {
-  render() {
-    const { plugin } = this.props;
-
-    return (
-      <InstanceProvider
-        metricInstanceName={plugin.meta.jsonData?.metrics?.grafanaName}
-        logsInstanceName={plugin.meta.jsonData?.logs?.grafanaName}
-        meta={plugin.meta}
-      >
+export const ConfigPageWrapper = ({ plugin }: Props) => {
+  return (
+    <InstanceProvider
+      metricInstanceName={plugin.meta.jsonData?.metrics?.grafanaName}
+      logsInstanceName={plugin.meta.jsonData?.logs?.grafanaName}
+      meta={plugin.meta}
+    >
+      <QueryClientProvider client={queryClient}>
         <ConfigPage />
-      </InstanceProvider>
-    );
-  }
-}
+      </QueryClientProvider>
+    </InstanceProvider>
+  );
+};
