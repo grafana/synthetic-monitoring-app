df99af84cf02ff0ad3530d06686acc49d9e58d79
Virginia Cepeda <virginia.cepeda@grafana.com>
2024-05-30 21:59:37 -0300

    feat: create hook to be in charge of filter configuration
    - Probes encode/decode functions still need a refactor
    - The idea is it doesn't rely on JSON.stringify/parse

---
+++ src/hooks/useCheckFilters.ts
@@ -0,0 +1,69 @@
+import { SelectableValue } from '@grafana/data';
+import { capitalize } from 'lodash';
+
+import { CheckEnabledStatus, CheckTypeFilter } from 'types';
+import { defaultFilters } from 'components/CheckFilters';
+
+import useQueryParametersState from './useQueryParametersState';
+
+export type FilterType = 'search' | 'labels' | 'type' | 'status' | 'probes';
+
+interface CheckFiltersProps {
+  search: [state: string, update: (value: string | null) => void];
+  labels: [state: string[], update: (value: string[] | null) => void];
+  type: [state: CheckTypeFilter, update: (value: CheckTypeFilter | null) => void];
+  status: [
+    state: SelectableValue<CheckEnabledStatus>,
+    update: (value: SelectableValue<CheckEnabledStatus> | null) => void
+  ];
+  probes: [
+    state: Array<SelectableValue<{ label: string; value: number }>>,
+    update: (value: Array<SelectableValue<{ label: string; value: number }>> | null) => void
+  ];
+}
+
+export function useCheckFilters() {
+  const filters: CheckFiltersProps = {
+    search: useQueryParametersState<string>({
+      key: 'search',
+      initialValue: defaultFilters.search,
+      encode: (value) => value,
+      decode: (value) => value,
+    }),
+    labels: useQueryParametersState<string[]>({
+      key: 'labels',
+      initialValue: defaultFilters.labels,
+      encode: (value) => value.join(','),
+      decode: (value) => value.split(','),
+    }),
+    type: useQueryParametersState<CheckTypeFilter>({
+      key: 'type',
+      initialValue: defaultFilters.type,
+      encode: (value) => value,
+      decode: (value) => value as CheckTypeFilter,
+    }),
+    status: useQueryParametersState<SelectableValue<CheckEnabledStatus>>({
+      key: 'status',
+      initialValue: defaultFilters.status,
+      encode: (value) => value.value || '',
+      decode: (value) => {
+        if (Object.values(CheckEnabledStatus).includes(value as CheckEnabledStatus)) {
+          return { label: capitalize(value), value: value as CheckEnabledStatus };
+        }
+        return { label: capitalize(CheckEnabledStatus.All), value: CheckEnabledStatus.All };
+      },
+    }),
+    probes: useQueryParametersState<Array<SelectableValue<{ label: string; value: number }>>>({
+      key: 'probes',
+      initialValue: defaultFilters.probes,
+      encode: (value) => {
+        return JSON.stringify(value);
+      },
+      decode: (value) => {
+        return JSON.parse(value);
+      },
+    }),
+  };
+
+  return filters;
+}
