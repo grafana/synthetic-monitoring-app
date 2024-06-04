bbf9c5e9b43e8eaef0c7915762021ea97d3f72ad
Chris Bedwell <christopher.bedwell@grafana.com>
2024-06-04 13:58:18 +0100

    Update src/components/CheckEditor/FormComponents/HttpCheckRegExValidation.tsx
    Co-authored-by: Virginia Cepeda <virginiamcepeda@gmail.com>

--- src/components/CheckEditor/FormComponents/HttpCheckRegExValidation.tsx
+++ src/components/CheckEditor/FormComponents/HttpCheckRegExValidation.tsx
@@ -94,7 +94,7 @@ export const HttpCheckRegExValidation = () => {
                   )}
                   <Field
                     className={cx(styles.field, styles.grow)}
-                    invalid={Boolean(baseErrorPath?.expression) ? true : false}
+                    invalid={Boolean(baseErrorPath?.expression)}
                     error={baseErrorPath?.expression?.message}
                   >
                     <Input
