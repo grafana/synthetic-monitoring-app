fdf06049e48d40e75da71ca375953580a2366b6c
Virginia Cepeda <virginiamcepeda@gmail.com>
2024-07-02 13:42:27 -0300

    feat: improve non-initialized pages (#839)
    * feat: create hook to initialize app
    
    * feat: add component to handle button and modal for app initialization
    
    * feat: use AppInitializer from WelcomePage and ChecksWelcomePage
    
    * feat: welcome pages for probes and alerting
    
    * feat: add welcome pages in routing
    
    * chore: fix lint
    
    * chore: add docs link to empty scenes view
    
    * fix: update tests
    
    * fix: remove empty sections in config when uninitialized
    
    * fix: lint
    
    * fix: minor fixes to welcome pages
    
    - created component to reuse code and make sure it's consistent
    - applied max-width
    - set button text as constants
    
    * fix: change hook to named export
    
    * fix: checks welcome page text and test

--- src/components/ProgrammaticManagement.tsx
+++ src/components/ProgrammaticManagement.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
+import { InstanceContext } from 'contexts/InstanceContext';
 import { AccessToken } from 'components/AccessToken';
 import { TerraformConfig } from 'components/TerraformConfig';
 
 export const ProgrammaticManagement = () => {
+  const { meta, instance } = useContext(InstanceContext);
+  const initialized = meta?.enabled && instance.api;
+
   return (
     <div>
       <h3>Programmatic management</h3>
-      <AccessToken />
+      {initialized && <AccessToken />}
       <br />
       <TerraformConfig />
     </div>
--- src/scenes/Common/emptyScene.tsx
+++ src/scenes/Common/emptyScene.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { GrafanaTheme2 } from '@grafana/data';
 import { EmbeddedScene, SceneReactObject } from '@grafana/scenes';
-import { Button, Card, useStyles2 } from '@grafana/ui';
+import { Button, Card, TextLink, useStyles2 } from '@grafana/ui';
 import { css } from '@emotion/css';
 
 import { CheckType, ROUTES } from 'types';
@@ -22,7 +22,7 @@ function getStyles(theme: GrafanaTheme2) {
     `,
     emptyCard: css`
       min-height: 200px;
-      max-width: 600px;
+      max-width: 800px;
       padding: ${theme.spacing(4)};
     `,
     cardButtons: css`
@@ -40,7 +40,13 @@ function EmptyScene({ checkType }: { checkType?: CheckType }) {
     <div className={styles.container}>
       <Card className={styles.emptyCard}>
         <Card.Heading className={styles.cardHeader}>
-          You don&apos;t have any {checkType ? checkType.toUpperCase() : ''} checks running
+          <p>
+            You don&apos;t have any {checkType ? checkType.toUpperCase() : ''} checks running. Click the Create a check
+            button to start monitoring your services with Grafana Cloud, or{' '}
+            <TextLink href="https://grafana.com/docs/grafana-cloud/synthetic-monitoring/" external={true}>
+              check out the Synthetic Monitoring docs.
+            </TextLink>
+          </p>
         </Card.Heading>
         <Card.Actions className={styles.cardButtons}>
           <Button
