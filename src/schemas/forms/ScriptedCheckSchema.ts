ccb8d2021e7e404a0865629e714651130c6a567e
Virginia Cepeda <virginia.cepeda@grafana.com>
2024-07-17 16:01:03 -0300

    feat: apply browser and script validations to form

--- src/schemas/forms/ScriptedCheckSchema.ts
+++ src/schemas/forms/ScriptedCheckSchema.ts
@@ -2,29 +2,11 @@ import { z, ZodType } from 'zod';
 
 import { CheckFormValuesScripted, CheckType, ScriptedSettings } from 'types';
 
+import { maxSizeValidation, validateNonBrowserScript } from './script/validation';
 import { BaseCheckSchema } from './BaseCheckSchema';
 
-const MAX_SCRIPT_IN_KB = 128;
-
-/** Important: this schema is also used in `BrowserCheckSchema.ts`
- *  If you change this schema, make sure to update the other file as well (if they start to diverge).
- * */
 export const ScriptedSettingsSchema: ZodType<ScriptedSettings> = z.object({
-  script: z
-    .string()
-    .min(1, `Script is required.`)
-    .superRefine((val, ctx) => {
-      const textBlob = new Blob([val], { type: 'text/plain' });
-      const sizeInBytes = textBlob.size;
-      const sizeInKb = sizeInBytes / 1024;
-
-      if (sizeInKb > MAX_SCRIPT_IN_KB) {
-        return ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: `Script is too big (${sizeInKb.toFixed(2)}kb). Maximum size is ${MAX_SCRIPT_IN_KB}kb.`,
-        });
-      }
-    }),
+  script: z.string().min(1, `Script is required.`).superRefine(maxSizeValidation).superRefine(validateNonBrowserScript),
 });
 
 const ScriptedSchemaValues = z.object({
