6d8406f8e0f78f3c177de6cf09c976ad8057decd
Virginia Cepeda <virginia.cepeda@grafana.com>
2024-07-19 16:59:31 -0300

    feat: redirect to homepage when viewer tries to go to edit page

--- src/page/CheckRouter.test.tsx
+++ src/page/CheckRouter.test.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { OrgRole } from '@grafana/data';
+import { config } from '@grafana/runtime';
 import { screen, waitFor } from '@testing-library/react';
 import { BASIC_HTTP_CHECK, BASIC_SCRIPTED_CHECK } from 'test/fixtures/checks';
 import { render } from 'test/render';
@@ -9,6 +11,14 @@ import { PLUGIN_URL_PATH } from 'components/Routing.consts';
 import { CheckRouter } from './CheckRouter';
 
 describe(`<CheckRouter />`, () => {
+  beforeAll(() => {
+    jest.replaceProperty(config, 'bootData', {
+      // @ts-expect-error
+      user: {
+        orgRole: OrgRole.Editor,
+      },
+    });
+  });
   it(`should redirect from the old add new check route to the new one`, async () => {
     const checkType = CheckType.HTTP;
 
@@ -65,5 +75,52 @@ describe(`<CheckRouter />`, () => {
 
     const title = await screen.findByText(`Editing ${BASIC_SCRIPTED_CHECK.job}`);
     expect(title).toBeInTheDocument();
+  });
+
+  describe('Permissions', () => {
+    describe('When user is viewer', () => {
+      beforeAll(() => {
+        jest.replaceProperty(config, 'bootData', {
+          // @ts-expect-error
+          user: {
+            orgRole: OrgRole.Viewer,
+          },
+        });
+      });
+      it(`Should not load the edit check route and redirect to the homepage`, async () => {
+        const checkType = CheckType.Scripted;
+        const checkID = BASIC_SCRIPTED_CHECK.id;
+
+        const { history } = render(<CheckRouter />, {
+          path: `${PLUGIN_URL_PATH}${ROUTES.Checks}/edit/${checkType}/${checkID}`,
+          route: `${PLUGIN_URL_PATH}${ROUTES.Checks}`,
+        });
+
+        await waitFor(() => expect(history.location.pathname).toBe(`${PLUGIN_URL_PATH}${ROUTES.Checks}`));
+      });
+    });
+
+    describe('When user is editor', () => {
+      beforeAll(() => {
+        jest.replaceProperty(config, 'bootData', {
+          // @ts-expect-error
+          user: {
+            orgRole: OrgRole.Editor,
+          },
+        });
+      });
+      it(`Should load the edit check route`, async () => {
+        const checkType = CheckType.Scripted;
+        const checkID = BASIC_SCRIPTED_CHECK.id;
+
+        render(<CheckRouter />, {
+          path: `${PLUGIN_URL_PATH}${ROUTES.Checks}/edit/${checkType}/${checkID}`,
+          route: `${PLUGIN_URL_PATH}${ROUTES.Checks}`,
+        });
+
+        const title = await screen.findByText(`Editing ${BASIC_SCRIPTED_CHECK.job}`);
+        expect(title).toBeInTheDocument();
+      });
+    });
   });
 });
