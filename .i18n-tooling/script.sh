4a119ecccd0e0db8365e39d06b418c1f8974c691
Joey Tawadrous <joey.tawadrous@grafana.com>
2024-08-06 09:06:37 +0100

    feat: update betterer script to ignore new package and updated comments

--- .i18n-tooling/script.sh
+++ .i18n-tooling/script.sh
@@ -4,16 +4,23 @@ read input
 echo $input
 
 if [ $input == "y" ]; then
-  echo "-----------------------------------"
+  # Copy packages/grafana-eslint-rules to root directory
+  # This is necessary for betterer to find the rules
+  echo ""
   echo "Copying i18n-tooling/packages/grafana-eslint-rules to root directory..."
   mkdir -p ./packages/grafana-eslint-rules
   cp -r ./.i18n-tooling/packages/grafana-eslint-rules ./packages
 
-  echo "-----------------------------------"
+  # Add packages directory to .gitignore
+  echo "" >> .gitignore
+  echo "# Directory for i18n eslint rules" >> .gitignore
+  echo "packages/" >> .gitignore
+
+  echo ""
   echo "Installing betterer..."
   # npm install -g betterer
   echo "Betterer installed!"
   echo "You can now run 'betterer init' to create a betterer config file."
 else
-  echo "Betterer not installed."
+  echo "Betterer not set up."
 fi
