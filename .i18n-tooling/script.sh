f0d15200b4a16591f7816a287247819c18924b5e
Joey Tawadrous <joey.tawadrous@grafana.com>
2024-08-06 08:41:09 +0100

    feat: copy eslint rules to root dir

--- .i18n-tooling/script.sh
+++ .i18n-tooling/script.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-echo -n "Would you like to install betterer? (y/n): "
+echo -n "Would you like to set up betterer? (y/n): "
 read input
 echo $input
+
+if [ $input == "y" ]; then
+  echo "-----------------------------------"
+  echo "Copying i18n-tooling/packages/grafana-eslint-rules to root directory..."
+  mkdir -p ./packages/grafana-eslint-rules
+  cp -r ./.i18n-tooling/packages/grafana-eslint-rules ./packages
+
+  echo "-----------------------------------"
+  echo "Installing betterer..."
+  # npm install -g betterer
+  echo "Betterer installed!"
+  echo "You can now run 'betterer init' to create a betterer config file."
+else
+  echo "Betterer not installed."
+fi
