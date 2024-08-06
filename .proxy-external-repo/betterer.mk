591168cff898d58e0a5c1c9964153218fe2dc8f7
Marcelo E. Magallon <marcelo.magallon@grafana.com>
2024-08-06 16:21:50 -0600

    feat: split makefile so that it can be modified
    Signed-off-by: Marcelo E. Magallon <marcelo.magallon@grafana.com>

---
+++ .proxy-external-repo/betterer.mk
@@ -0,0 +1,18 @@
+.PHONY: i18n-betterer-setup
+i18n-betterer-setup:
+	betterer ./.config-i18n
+
+.PHONY: i18n-betterer-cicd
+i18n-betterer-cicd:
+	betterer --update --silent
+
+.PHONY: i18n-betterer-local
+i18n-betterer-local:
+	@echo "Checking for betterer i18n strings"
+	rm -f .betterer.results-temp
+	cp .betterer.results .betterer.results-temp
+	# run betterer and if it errors remove the temp file
+	betterer --results .betterer.results-temp || rm -f .betterer.results-temp
+
+	# if it succeeds remove the temp file
+	rm -f .betterer.results-temp
---
+++ .proxy-external-repo/crowdin.mk
@@ -0,0 +1,10 @@
+.PHONY: i18n-upload
+i18n-upload:
+	@echo "Uploading source files to crowdin"
+	crowdin config lint
+	crowdin upload sources
+
+.PHONY: i18n-download
+i18n-download:
+	@echo "Download translation files from crowdin"
+	crowdin pull --all
