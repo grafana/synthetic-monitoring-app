c7a2d11b0e9ca3b524ec505569446250af3c768d
Chris Bedwell <christopher.bedwell@grafana.com>
2024-08-07 13:52:25 +0100

    feat: add trans components to checkswelcomepage

--- src/page/ChecksWelcomePage.tsx
+++ src/page/ChecksWelcomePage.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { TextLink } from '@grafana/ui';
 
 import { ROUTES } from 'types';
+import { t, Trans } from 'components/i18n';
 
 import { SubsectionWelcomePage } from './SubsectionWelcomePage';
 
 export const ChecksWelcomePage = () => {
-  const BUTTON_TEXT = 'Create a Check';
+  const BUTTON_TEXT = t(`welcome.checks.cta`, 'Create a Check');
 
   return (
     <SubsectionWelcomePage redirectTo={ROUTES.ChooseCheckGroup} buttonText={BUTTON_TEXT}>
-      Click the {BUTTON_TEXT} button to initialize the plugin and create checks or visit the Synthetic Monitoring{' '}
-      <TextLink href="https://grafana.com/docs/grafana-cloud/testing/synthetic-monitoring/" external={true}>
-        documentation
-      </TextLink>{' '}
-      for more information
+      <Trans i18nKey={`welcome.checks.content`}>
+        Click the {{ buttonText: BUTTON_TEXT }} button to initialize the plugin and create checks or visit the Synthetic
+        Monitoring{' '}
+        <TextLink href="https://grafana.com/docs/grafana-cloud/testing/synthetic-monitoring/" external={true}>
+          documentation
+        </TextLink>{' '}
+        for more information
+      </Trans>
     </SubsectionWelcomePage>
   );
 };
