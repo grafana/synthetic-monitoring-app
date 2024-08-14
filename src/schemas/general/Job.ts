4ceebdd315f2865d0198ea415e0f97a546dda3b8
Virginia Cepeda <virginia.cepeda@grafana.com>
2024-08-14 16:38:30 -0300

    fix: add job name validation not to contain commas or single quotes

--- src/schemas/general/Job.ts
+++ src/schemas/general/Job.ts
@@ -5,4 +5,11 @@ export const JobSchema = z
     required_error: 'Job name is required',
   })
   .min(1, { message: 'Job name is required' })
-  .max(128, { message: 'Job name must be 128 characters or less' });
+  .max(128, { message: 'Job name must be 128 characters or less' })
+  .superRefine((value, ctx) => {
+    if (value.includes("'") || value.includes(',')) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: `Job names can't contain commas or single quotes`,
+      });
+    }})
