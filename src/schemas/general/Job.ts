e502eccf3ba332cfa28afe4b373a7290ab063261
Virginia Cepeda <virginia.cepeda@grafana.com>
2024-08-15 13:09:09 -0300

    fix: trim job names

--- src/schemas/general/Job.ts
+++ src/schemas/general/Job.ts
@@ -4,6 +4,7 @@ export const JobSchema = z
   .string({
     required_error: 'Job name is required',
   })
+  .trim()
   .min(1, { message: 'Job name is required' })
   .max(128, { message: 'Job name must be 128 characters or less' })
   .superRefine((value, ctx) => {
@@ -12,4 +13,5 @@ export const JobSchema = z
         code: z.ZodIssueCode.custom,
         message: `Job names can't contain commas or quotes`,
       });
-    }})
+    }
+  });
