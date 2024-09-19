25ace4d0d4959d1a527cffec9e40f3fb29a2c2cb
Virginia Cepeda <virginia.cepeda@grafana.com>
2024-09-19 12:29:01 -0300

    feat: add script examples to browser checks

---
+++ src/components/BrowserExamplesMenu/snippets/querying.js
@@ -0,0 +1,37 @@
+import { check } from 'k6';
+import { browser } from 'k6/browser';
+
+export const options = {
+  scenarios: {
+    ui: {
+      executor: 'shared-iterations',
+      options: {
+        browser: {
+          type: 'chromium',
+        },
+      },
+    },
+  },
+  thresholds: {
+    checks: ['rate==1.0'],
+  },
+};
+
+export default async function () {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  try {
+    await page.goto('https://test.k6.io/');
+
+    const titleWithCSS = await page.$('header h1.title').then((e) => e.textContent());
+    const titleWithXPath = await page.$(`//header//h1[@class="title"]`).then((e) => e.textContent());
+
+    check(page, {
+      'Title with CSS selector': titleWithCSS == 'test.k6.io',
+      'Title with XPath selector': titleWithXPath == 'test.k6.io',
+    });
+  } finally {
+    await page.close();
+  }
+}
