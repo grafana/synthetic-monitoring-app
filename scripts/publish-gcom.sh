55d0a46866d7a330366557adece8bfb1f161f640
ka3de <danijs12@hotmail.com>
2024-10-09 14:41:42 +0200

    debug ci

--- scripts/publish-gcom.sh
+++ scripts/publish-gcom.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 # This is used in Drone to generate the version string for automatic PR creation
 set -eufo pipefail
 
@@ -7,8 +7,10 @@ VERSION="$(grep version ${ROOT_DIR}/package.json | cut -d':' -f2 | tr -d "\"', \
 
 echo "${VERSION}"
 
-curl -f -w "status=%{http_code}" -s -H "Authorization: Bearer ${GRAFANA_API_KEY}" \
--d "download[any][url]=https://storage.googleapis.com/synthetic-monitoring-app-prod/builds/${VERSION}/grafana-synthetic-monitoring-app-${VERSION}.zip" \
--d "download[any][md5]=$$(curl -sL https://storage.googleapis.com/synthetic-monitoring-app-prod/builds/${VERSION}/grafana-synthetic-monitoring-app-${VERSION}.zip | md5sum | cut -d' ' -f1)" \
--d url=https://github.com/grafana/synthetic-monitoring-app \
-"https://grafana.com/api/plugins"
+curl -I https://grafana.com
+
+#curl -f -w "status=%{http_code}" -s -H "Authorization: Bearer ${GRAFANA_API_KEY}" \
+#-d "download[any][url]=https://storage.googleapis.com/synthetic-monitoring-app-prod/builds/${VERSION}/grafana-synthetic-monitoring-app-${VERSION}.zip" \
+#-d "download[any][md5]=$$(curl -sL https://storage.googleapis.com/synthetic-monitoring-app-prod/builds/${VERSION}/grafana-synthetic-monitoring-app-${VERSION}.zip | md5sum | cut -d' ' -f1)" \
+#-d url=https://github.com/grafana/synthetic-monitoring-app \
+#"https://grafana.com/api/plugins"
