56b8f34b6a481e5f1ac8c38075ec7389fd7a8c20
Virginia Cepeda <virginia.cepeda@grafana.com>
2024-10-31 15:45:13 -0300

    feat: add useLocalStorage hook and store dismissed  private probe alert

--- src/components/CheckEditor/CheckProbes/PrivateProbesAlert.tsx
+++ src/components/CheckEditor/CheckProbes/PrivateProbesAlert.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import { Alert, LinkButton, Stack } from '@grafana/ui';
+import React from 'react';
+import { Alert, LinkButton, Stack, TextLink } from '@grafana/ui';
 
 import { ROUTES } from 'types';
+import { useLocalStorage } from 'hooks/useLocalStorage';
 import { getRoute } from 'components/Routing.utils';
 
 export const PrivateProbesAlert = () => {
-  const [display, setDisplay] = useState(true);
+  const [dismissed, setDismissed] = useLocalStorage<boolean>('dismissedPrivateProbesAlert', false);
 
-  if (!display) {
+  if (dismissed) {
     return null;
   }
 
@@ -16,12 +17,17 @@ export const PrivateProbesAlert = () => {
       title="You haven't set up any private probes yet."
       severity="info"
       onRemove={() => {
-        setDisplay(false);
+        setDismissed(true);
       }}
     >
       <Stack gap={1} direction={'column'} alignItems={'flex-start'}>
-        Private probes are instances of the open source Grafana Synthetic Monitoring Agent and are only accessible to
-        you.
+        <p>
+          Private probes are instances of the open source Grafana{' '}
+          <TextLink href="https://github.com/grafana/synthetic-monitoring-agent" external={true}>
+            Synthetic Monitoring Agent
+          </TextLink>{' '}
+          and are only accessible to you.
+        </p>
         <LinkButton size="sm" href={`${getRoute(ROUTES.NewProbe)}`}>
           Set up a Private Probe
         </LinkButton>
