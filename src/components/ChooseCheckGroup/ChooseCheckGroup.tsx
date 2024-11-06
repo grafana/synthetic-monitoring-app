f7623a75818fdc9f863cc08ad62195c9c42071f2
Virginia Cepeda <virginia.cepeda@grafana.com>
2024-11-06 16:28:28 -0300

    fix: add container queries

--- src/components/ChooseCheckGroup/ChooseCheckGroup.tsx
+++ src/components/ChooseCheckGroup/ChooseCheckGroup.tsx
@@ -16,26 +16,47 @@ export const ChooseCheckGroup = () => {
 
   return (
     <PluginPage layout={PageLayoutType.Standard} pageNav={{ text: 'Choose a check type' }}>
-      <Stack direction={`column`} gap={2}>
-        <div>
-          Pick between {options.length} different types of checks to monitor your services. Choose the one that best
-          fits your needs.
-        </div>
-        <OverLimitAlert />
-        <div className={styles.container} data-testid={DataTestIds.CHOOSE_CHECK_TYPE}>
-          {options.map((group) => {
-            return <CheckGroupCard key={group.label} group={group} />;
-          })}
-        </div>
-      </Stack>
+      <div className={styles.wrapper}>
+        <Stack direction="column" gap={2}>
+          <div>
+            Pick between {options.length} different types of checks to monitor your services. Choose the one that best
+            fits your needs.
+          </div>
+          <OverLimitAlert />
+          <div className={styles.container} data-testid={DataTestIds.CHOOSE_CHECK_TYPE}>
+            {options.map((group) => {
+              return <CheckGroupCard key={group.label} group={group} />;
+            })}
+          </div>
+        </Stack>
+      </div>
     </PluginPage>
   );
 };
 
 const getStyles = (theme: GrafanaTheme2) => {
-  const twoColumnsQuery = `@media (max-width: ${theme.breakpoints.values.xxl}px)`;
-  const oneColumnQuery = `@media (max-width: ${theme.breakpoints.values.sm}px)`;
+  const containerName = `checkGroupContainer`;
+  const oneColQuery = `(max-width: ${theme.breakpoints.values.sm}px)`;
+  const twoColsQuery = `(max-width: ${theme.breakpoints.values.xxl}px)`;
+
+  const containerOneColQuery = `@container ${containerName} ${oneColQuery}`;
+  const containerTwoColsQuery = `@container ${containerName} ${twoColsQuery}`;
+  const oneColMediaQuery = `@supports not (container-type: inline-size) @media ${oneColQuery}`;
+  const twoColsMediaQuery = `@supports not (container-type: inline-size) @media ${twoColsQuery}`;
+
+  const containerRules = {
+    twoCols: `repeat(2, 1fr)`,
+    oneCol: '1fr',
+  };
+
   return {
+    wrapper: css({
+      containerName,
+      containerType: `inline-size`,
+      height: '100%',
+      contain: 'layout',
+    }),
+
     container: css({
       width: `100%`,
       display: `grid`,
@@ -44,12 +65,18 @@ const getStyles = (theme: GrafanaTheme2) => {
       textAlign: `center`,
       color: theme.colors.text.secondary,
 
-      [twoColumnsQuery]: {
-        gridTemplateColumns: 'repeat(2, 1fr)',
+      [twoColsMediaQuery]: {
+        gridTemplateColumns: containerRules.twoCols,
+      },
+      [containerTwoColsQuery]: {
+        gridTemplateColumns: containerRules.twoCols,
       },
 
-      [oneColumnQuery]: {
-        gridTemplateColumns: '1fr',
+      [oneColMediaQuery]: {
+        gridTemplateColumns: containerRules.oneCol,
+      },
+      [containerOneColQuery]: {
+        gridTemplateColumns: containerRules.oneCol,
       },
     }),
   };
