2cda688c440068f559a0b65cc345e974f6df0734
Thomas <thomas@w1kman.se>
2024-11-21 09:32:10 +0100

    chore: make note of duplication

--- src/page/NewCheck/__tests__/MultiStepChecks/MultiHTTP/4-alerting.payload.test.tsx
+++ src/page/NewCheck/__tests__/MultiStepChecks/MultiHTTP/4-alerting.payload.test.tsx
@@ -7,6 +7,7 @@ import { fillMandatoryFields } from '../../../../__testHelpers__/multiStep';
 
 const checkType = CheckType.MULTI_HTTP;
 
+// TODO: Remove/keep? It's a duplicate of the one in the `CommonFields.payload.test.tsx`
 describe(`Section 4 (alerting)`, () => {
   it(`can submit the form with alerting filled in`, async () => {
     const { user, read } = await renderNewForm(checkType);
