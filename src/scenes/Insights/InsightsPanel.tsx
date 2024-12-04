65de92704d9a45f080f9408417cdc3c2eb12359e
Vicente Ortega Torres <vicente.ortega@grafana.com>
2024-12-04 14:21:00 +0100

    Refactor src/scenes/Insights/InsightsPanel.tsx

--- src/scenes/Insights/InsightsPanel.tsx
+++ src/scenes/Insights/InsightsPanel.tsx
@@ -8,15 +8,16 @@ import {
   SceneObjectState,
 } from '@grafana/scenes';
 import { getInsightsPanelStyles } from './getInsightsPanelStyles';
-import {getInsightsItem} from "./InsightsItem";
-import {DataSourceRef} from "@grafana/schema";
-import {getInsightsMetric} from "./insightsMetrics";
+import { getInsightsItem } from './InsightsItem';
+import { DataSourceRef } from '@grafana/schema';
+import { getInsightsMetric } from './insightsMetrics';
 
 interface InsightsState extends SceneObjectState {
-  insights: Array<{ title: string; description: string; link?: string; linkText?: string }>;
+  audits: Array<{ id: string; title: string; description: string; link?: string; linkText?: string }>;
 }
 
 interface Insight {
+  id: string;
   title: string;
   description: string;
   link?: string;
@@ -28,7 +29,7 @@ export class Insights extends SceneObjectBase<InsightsState> {
 
   public constructor() {
     super({
-      insights: [],
+      audits: [],
     });
 
     this.initializeFakeInsights();
@@ -36,12 +37,27 @@ export class Insights extends SceneObjectBase<InsightsState> {
 
   private initializeFakeInsights() {
     const fakeInsights: Insight[] = [
-      { title: 'High metric cardinality', description: 'Our algorithms have detected high metric cardinality, meaning your test is generating an excessive number of unique values for tags.', link: 'https://grafana.com/docs/k6/latest/using-k6/http-requests/#url-grouping', linkText: 'Learn more about URL grouping' },
-      { title: 'Increment in Average Latency', description: 'The average latency of the Checks increased by 20% over the last month.' },
-      { title: 'Increment in Probe Latency', description: 'The average latency of the Probe in Atlanta increased by 10% over the last month.' },
+      {
+        id: 'high-cardinality',
+        title: 'High metric cardinality',
+        description:
+            'Our algorithms have detected high metric cardinality, meaning your test is generating an excessive number of unique values for tags.',
+        link: 'https://grafana.com/docs/k6/latest/using-k6/http-requests/#url-grouping',
+        linkText: 'Learn more about URL grouping',
+      },
+      {
+        id: 'average-latency-increased-last-month',
+        title: 'Average Latency: Increased by 20%',
+        description: 'The average latency of the Checks increased by 20% over the last month.',
+      },
+      {
+        id: 'probe-latency-increased-last-week',
+        title: 'Probe Latency: Increased by 10%',
+        description: 'The average latency of the Probe in Atlanta increased by 10% over the last month.',
+      },
     ];
 
-    this.setState({ insights: fakeInsights });
+    this.setState({ audits: fakeInsights });
   }
 }
 
@@ -59,53 +75,92 @@ function InsightsRenderer({ model }: SceneComponentProps<Insights>) {
   );
 }
 
+function getHighCardinalityInsightsItem(title: string, description: string, link: string, linkText: string) {
+  return new SceneFlexItem({
+    height: 200,
+    body: getInsightsItem(title, description, link, linkText),
+  });
+}
+
+function getAverageLatencyLastMonthItem(metrics: DataSourceRef, title: string, description: string) {
+  return new SceneFlexItem({
+    width: 650,
+    height: 200,
+    body: getInsightsMetric(
+        metrics,
+        title,
+        description,
+        [
+          {
+            expr: `sum by (job) (probe_http_total_duration_seconds{job=~".*"})`,
+            refId: 'A',
+            legendFormat: '{{probe}}',
+          },
+          {
+            expr: `sum by (job) (probe_http_total_duration_seconds{job=~".*"} offset 30d)`,
+            refId: 'B',
+            legendFormat: '{{job}} - 30d',
+          },
+        ]
+    ),
+  });
+}
+
+function getProbeLatencyLastMonthItem(metrics: DataSourceRef, title: string, description: string) {
+  return new SceneFlexItem({
+    width: 650,
+    height: 200,
+    body: getInsightsMetric(
+        metrics,
+        title,
+        description,
+        [
+          {
+            expr: `sum by (probe) (probe_http_total_duration_seconds{probe=~".*"})`,
+            refId: 'A',
+            legendFormat: '{{probe}}',
+          },
+          {
+            expr: `sum by (probe) (probe_http_total_duration_seconds{probe=~".*"} offset 7d)`,
+            refId: 'B',
+            legendFormat: '{{probe}} - 7d',
+          },
+        ]
+    ),
+  });
+}
+
 export function getInsightsPanel(metrics: DataSourceRef) {
+  const insights = new Insights();
+  const audits = insights.state.audits; // Accessing the state of the Insights component directly
+
+  const insightsItems = audits.map((insight) => {
+    if (insight.id === 'high-cardinality') {
+      return getHighCardinalityInsightsItem(
+          insight.title,
+          insight.description,
+          insight.link!,
+          insight.linkText!
+      );
+    }
+    if (insight.id === 'average-latency-increased-last-month') {
+      return getAverageLatencyLastMonthItem(metrics, insight.title, insight.description);
+    }
+    if (insight.id === 'probe-latency-increased-last-week') {
+      return getProbeLatencyLastMonthItem(metrics, insight.title, insight.description);
+    }
+    return null;
+  }).filter(Boolean) as SceneFlexItem[];
+
   return new SceneFlexLayout({
     direction: 'column',
     children: [
       new SceneFlexItem({
-        body: new Insights(),
+        body: insights,
       }),
       new SceneFlexLayout({
         direction: 'row',
-        children: [
-          new SceneFlexItem({
-            height: 200,
-            body: getInsightsItem('High metric cardinality', 'Our algorithms have detected high metric cardinality, meaning your test is generating an excessive number of unique values for tags.', 'https://grafana.com/docs/k6/latest/using-k6/http-requests/#url-grouping', 'Learn more about URL grouping'),
-          }),
-          new SceneFlexItem({
-            width: 650,
-            height: 200,
-            body: getInsightsMetric(metrics, 'Average Latency: Increased by 20%', 'The average latency of the Checks increased by 20% over the last month.', [
-              {
-                expr: `sum by (job) (probe_http_total_duration_seconds{job=~".*"})`,
-                refId: 'A',
-                legendFormat: '{{probe}}',
-              },
-              {
-                expr: `sum by (job) (probe_http_total_duration_seconds{job=~".*"} offset 30d)`,
-                refId: 'B',
-                legendFormat: '{{job}} - 30d',
-              },
-            ]),
-          }),
-          new SceneFlexItem({
-            width: 650,
-            height: 200,
-            body: getInsightsMetric(metrics, 'Probe Latency: Increased by 10%', 'The average latency of the Probe in Atlanta increased by 10% over the last week.', [
-              {
-                expr: `sum by (probe) (probe_http_total_duration_seconds{probe=~".*"})`,
-                refId: 'A',
-                legendFormat: '{{probe}}',
-              },
-              {
-                expr: `sum by (probe) (probe_http_total_duration_seconds{probe=~".*"} offset 7d)`,
-                refId: 'B',
-                legendFormat: '{{probe}} - 7d',
-              },
-            ]),
-          }),
-        ],
+        children: insightsItems,
       }),
     ],
   });
