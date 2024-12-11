aa38510d9869ec306e3c480e36eb81945b706c21
Virginia Cepeda <virginia.cepeda@grafana.com>
2024-12-11 17:03:10 -0300

    test: add mocks back temporarily
    - since the alerts API is no longer in dev, I'm adding the mocks back to be able to test it
    - This should be improved by https://github.com/grafana/synthetic-monitoring-app/issues/850

---
+++ src/datasource/__mocks__/checkAlerts.ts
@@ -0,0 +1,41 @@
+import { CheckAlertType } from 'types';
+import { CheckAlertsResponse } from 'datasource/responses.types';
+import { queryClient } from 'data/queryClient';
+import { queryKeys } from 'data/useCheckAlerts';
+
+export function mockAlertsForCheckData(mockData: CheckAlertsResponse = alertsFromApi) {
+  queryClient.setQueryData([queryKeys.listAlertsForCheck], mockData);
+}
+
+export const alertsFromApi: CheckAlertsResponse = {
+  alerts: [
+    {
+      id: 1,
+      name: CheckAlertType['HTTPRequestDurationTooHighP90'],
+      threshold: 350,
+      created: 1724854935,
+      modified: 1724854935,
+    },
+    {
+      id: 2,
+      name: CheckAlertType['HTTPRequestDurationTooHighP95'],
+      threshold: 100,
+      created: 1724854935,
+      modified: 1724854935,
+    },
+    {
+      id: 3,
+      name: CheckAlertType['HTTPTargetCertificateCloseToExpiring'],
+      threshold: 90,
+      created: 1724854935,
+      modified: 1724854935,
+    },
+    {
+      id: 4,
+      name: CheckAlertType['ProbeFailedExecutionsTooHigh'],
+      threshold: 20,
+      created: 1724854935,
+      modified: 1724854935,
+    },
+  ],
+};
