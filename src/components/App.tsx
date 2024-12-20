33dad643a6557bc22c22faf22817955e9da54755
Virginia Cepeda <virginia.cepeda@grafana.com>
2024-12-20 15:39:06 -0300

    feat: introduce yarn dev:msw to start the app mocking the api using msw handlers

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppRootProps } from '@grafana/data';
 import { css, Global } from '@emotion/react';
-import { mockAlertsForCheckData } from 'datasource/__mocks__/checkAlerts';
+import { setupWorker } from 'msw';
 
 import { ProvisioningJsonData } from 'types';
 import { InitialisedRouter } from 'routing/InitialisedRouter';
@@ -13,6 +13,7 @@ import { SMDatasourceProvider } from 'contexts/SMDatasourceContext';
 import { queryClient } from 'data/queryClient';
 import { queryKeys as alertingQueryKeys } from 'data/useAlerts';
 
+import { handlers } from '../test/handlers';
 import { FeatureFlagProvider } from './FeatureFlagProvider';
 
 export const App = (props: AppRootProps<ProvisioningJsonData>) => {
@@ -28,9 +29,8 @@ export const App = (props: AppRootProps<ProvisioningJsonData>) => {
     };
   }, []);
 
-  //@todo REMOVE WHEN THE CHECK ALERTS API IS READY
-   if (process.env.NODE_ENV === 'development') {
-     mockAlertsForCheckData();
+  if (process.env.NODE_ENV === 'development' && process.env.REACT_APP_MSW) {
+    setupWorker(...handlers).start();
   }
 
   return (
