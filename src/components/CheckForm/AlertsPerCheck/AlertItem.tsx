b3fef5e46524884659e73216e5bf9da6a4554780
Virginia Cepeda <virginia.cepeda@grafana.com>
2025-01-14 16:04:30 -0300

    feat: add default values for check alerts

--- src/components/CheckForm/AlertsPerCheck/AlertItem.tsx
+++ src/components/CheckForm/AlertsPerCheck/AlertItem.tsx
@@ -27,7 +27,7 @@ export const AlertItem = ({
     onSelectionChange(type);
   };
 
-  const threshold: number = getValues(`alerts.${alert.type}.threshold`);
+  const threshold: number = getValues(`alerts.${alert.type}.threshold`) || alert.default;
   const thresholdError = formState.errors?.alerts?.[alert.type]?.threshold?.message;
 
   return (
--- src/components/CheckForm/AlertsPerCheck/AlertsPerCheck.constants.tsx
+++ src/components/CheckForm/AlertsPerCheck/AlertsPerCheck.constants.tsx
@@ -5,6 +5,7 @@ export interface PredefinedAlertInterface {
   name: string;
   unit: ThresholdUnit;
   category: CheckAlertCategory;
+  default?: number;
 }
 
 const GLOBAL_PREDEFINED_ALERTS: PredefinedAlertInterface[] = [
@@ -13,6 +14,7 @@ const GLOBAL_PREDEFINED_ALERTS: PredefinedAlertInterface[] = [
     name: 'Probe Failed Executions Too High',
     unit: '%',
     category: CheckAlertCategory.SystemHealth,
+    default: 10,
   },
 ];
 
@@ -22,30 +24,35 @@ const HTTP_PREDEFINED_ALERTS: PredefinedAlertInterface[] = [
     name: 'HTTP Request Duration Too High (P50)',
     unit: 'ms',
     category: CheckAlertCategory.RequestDuration,
+    default: 300,
   },
   {
     type: CheckAlertType.HTTPRequestDurationTooHighP90,
     name: 'HTTP Request Duration Too High (P90)',
     unit: 'ms',
     category: CheckAlertCategory.RequestDuration,
+    default: 500,
   },
   {
     type: CheckAlertType.HTTPRequestDurationTooHighP95,
     name: 'HTTP Request Duration Too High (P95)',
     unit: 'ms',
     category: CheckAlertCategory.RequestDuration,
+    default: 800,
   },
   {
     type: CheckAlertType.HTTPRequestDurationTooHighP99,
     name: 'HTTP Request Duration Too High (P99)',
     unit: 'ms',
     category: CheckAlertCategory.RequestDuration,
+    default: 1500,
   },
   {
     type: CheckAlertType.HTTPTargetCertificateCloseToExpiring,
     name: 'HTTP Target Certificate Close To Expiring',
     unit: 'd',
     category: CheckAlertCategory.SystemHealth,
+    default: 60,
   },
 ];
 
@@ -55,24 +62,28 @@ const PING_PREDEFINED_ALERTS: PredefinedAlertInterface[] = [
     name: 'Ping ICMP Duration Too High (P50)',
     unit: 'ms',
     category: CheckAlertCategory.RequestDuration,
+    default: 50,
   },
   {
     type: CheckAlertType.PingICMPDurationTooHighP90,
     name: 'Ping ICMP Duration Too High (P90)',
     unit: 'ms',
     category: CheckAlertCategory.RequestDuration,
+    default: 100,
   },
   {
     type: CheckAlertType.PingICMPDurationTooHighP95,
     name: 'Ping ICMP Duration Too High (P95)',
     unit: 'ms',
     category: CheckAlertCategory.RequestDuration,
+    default: 200,
   },
   {
     type: CheckAlertType.PingICMPDurationTooHighP99,
     name: 'Ping ICMP Duration Too High (P99)',
     unit: 'ms',
     category: CheckAlertCategory.RequestDuration,
+    default: 400,
   },
 ];
 
