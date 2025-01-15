12538967b44f89e8e8d50306552c7d0039aee9e0
Chris Bedwell <christopher.bedwell@grafana.com>
2025-01-15 13:10:05 +0000

    fix: update key value for check list

--- src/page/CheckList/CheckList.tsx
+++ src/page/CheckList/CheckList.tsx
@@ -213,7 +213,7 @@ const CheckListContent = ({ onChangeViewType, viewType }: CheckListContentProps)
               {currentPageChecks.map((check, index) => (
                 <CheckListItem
                   check={check}
-                  key={index}
+                  key={check.id}
                   onLabelSelect={handleLabelSelect}
                   onStatusSelect={handleStatusSelect}
                   onTypeSelect={handleTypeSelect}
