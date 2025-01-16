6114389e89e5f797473627655cd2ef0fba76b1c6
Virginia Cepeda <virginia.cepeda@grafana.com>
2025-01-16 12:13:00 -0300

    chore: remove checkAlert id concept
    The API is removing the id property for check alerts as they can be identified just by the name

--- src/datasource/__mocks__/checkAlerts.ts
+++ src/datasource/__mocks__/checkAlerts.ts
@@ -10,28 +10,24 @@ export function mockAlertsForCheckData(mockData: CheckAlertsResponse = alertsFro
 export const alertsFromApi: CheckAlertsResponse = {
   alerts: [
     {
-      id: 1,
       name: CheckAlertType['HTTPRequestDurationTooHighP90'],
       threshold: 350,
       created: 1724854935,
       modified: 1724854935,
     },
     {
-      id: 2,
       name: CheckAlertType['HTTPRequestDurationTooHighP95'],
       threshold: 100,
       created: 1724854935,
       modified: 1724854935,
     },
     {
-      id: 3,
       name: CheckAlertType['HTTPTargetCertificateCloseToExpiring'],
       threshold: 90,
       created: 1724854935,
       modified: 1724854935,
     },
     {
-      id: 4,
       name: CheckAlertType['ProbeFailedExecutionsTooHigh'],
       threshold: 20,
       created: 1724854935,
--- src/test/fixtures/checkAlerts.ts
+++ src/test/fixtures/checkAlerts.ts
@@ -4,28 +4,24 @@ import { CheckAlertsResponse } from 'datasource/responses.types';
 export const BASIC_CHECK_ALERTS: CheckAlertsResponse = {
   alerts: [
     {
-      id: 1,
       name: CheckAlertType['HTTPRequestDurationTooHighP90'],
       threshold: 350,
       created: 1724854935,
       modified: 1724854935,
     },
     {
-      id: 1,
       name: CheckAlertType['HTTPRequestDurationTooHighP95'],
       threshold: 100,
       created: 1724854935,
       modified: 1724854935,
     },
     {
-      id: 1,
       name: CheckAlertType['HTTPTargetCertificateCloseToExpiring'],
       threshold: 90,
       created: 1724854935,
       modified: 1724854935,
     },
     {
-      id: 1,
       name: CheckAlertType['ProbeFailedExecutionsTooHigh'],
       threshold: 20,
       created: 1724854935,
