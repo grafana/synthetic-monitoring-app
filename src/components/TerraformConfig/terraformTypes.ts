bc709e84122465389ef2a1003f1c8aa2f2af0046
Virginia Cepeda <virginia.cepeda@grafana.com>
2025-03-20 18:09:40 -0300

    fix: terraform config for multihttp

--- src/components/TerraformConfig/terraformTypes.ts
+++ src/components/TerraformConfig/terraformTypes.ts
@@ -9,7 +9,7 @@ import {
   TCPQueryResponse,
   TcpSettings,
 } from 'types';
-import { MultiHttpEntry, RequestProps } from 'components/MultiHttp/MultiHttpTypes';
+import { MultiHttpEntry, MultiHttpVariable, RequestProps } from 'components/MultiHttp/MultiHttpTypes';
 
 export interface TFOutput {
   config: TFConfig;
@@ -174,10 +174,35 @@ interface TFMultiHTTPSettings {
   entries: TFMultiHttpEntry[];
 }
 
-export interface TFMultiHttpEntry extends Omit<MultiHttpEntry, 'request'> {
-  request: TFMultiHttpRequest;
+export type TFMultiHTTPAssertionType = 'TEXT' | 'JSON_PATH_VALUE' | 'JSON_PATH_ASSERTION' | 'REGEX_ASSERTION';
+export type TFMultiHTTPAssertionCondition =
+  | 'NOT_CONTAINS'
+  | 'EQUALS'
+  | 'STARTS_WITH'
+  | 'ENDS_WITH'
+  | 'TYPE_OF'
+  | 'CONTAINS';
+export type TFMultiHttpAssertionSubject = 'RESPONSE_HEADERS' | 'HTTP_STATUS_CODE' | 'RESPONSE_BODY';
+
+export type TFMultiHTTPVariableType = 'JSON_PATH' | 'REGEX' | 'CSS_SELECTOR';
+
+export interface TFMultiHttpAssertion {
+  type: TFMultiHTTPAssertionType;
+  condition?: TFMultiHTTPAssertionCondition;
+  subject?: TFMultiHttpAssertionSubject;
+  expression?: string;
+  value?: string;
 }
 
+export interface TFMultiHttpVariable extends Omit<MultiHttpVariable, 'type'> {
+  type: TFMultiHTTPVariableType;
+}
+
+export interface TFMultiHttpEntry extends Omit<MultiHttpEntry, 'request' | 'checks' | 'variables'> {
+  request: TFMultiHttpRequest;
+  assertions?: TFMultiHttpAssertion[];
+  variables?: TFMultiHttpVariable[];
+}
 interface TFMultiHttpRequest extends Omit<RequestProps, 'queryFields' | 'postData' | 'body'> {
   query_fields?: Label[];
   post_data?: {
@@ -207,7 +232,8 @@ export interface TFProbeConfig {
   [key: string]: TFProbe;
 }
 
-export interface TFProbe extends Omit<Probe, 'online' | 'onlineChange' | 'version' | 'deprecated' | 'labels' | 'capabilities'> {
+export interface TFProbe
+  extends Omit<Probe, 'online' | 'onlineChange' | 'version' | 'deprecated' | 'labels' | 'capabilities'> {
   labels: TFLabels;
   disable_scripted_checks: boolean;
   disable_browser_checks: boolean;
