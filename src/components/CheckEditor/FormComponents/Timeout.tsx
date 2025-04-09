382d12b12f78e8e877338ae0ac8c484d554d505e
Chris Bedwell <christopher.bedwell@grafana.com>
2025-04-09 16:28:44 +0100

    feat: change controller component to usecontroller hook

--- src/components/CheckEditor/FormComponents/Timeout.tsx
+++ src/components/CheckEditor/FormComponents/Timeout.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Controller, useFormContext } from 'react-hook-form';
+import React, { useCallback } from 'react';
+import { useController, useFormContext } from 'react-hook-form';
 import { Field, Input } from '@grafana/ui';
 
 import { CheckFormValues, CheckType } from 'types';
@@ -17,10 +17,23 @@ export const Timeout = ({ checkType }: TimeoutProps) => {
     formState: { errors },
     control,
   } = useFormContext<CheckFormValues>();
+  const { field } = useController({ name: 'timeout', control });
   const { isFormDisabled } = useCheckFormContext();
   const min = MIN_TIMEOUT_MAP[checkType];
   const max = MAX_TIMEOUT_MAP[checkType];
   const readOnly = min === max;
+
+  const { ref, onChange, value, ...fieldProps } = field; // ref is unused, this is to silence warnings
+  const valueInSeconds = value / 1000;
+  const minInSeconds = min / 1000;
+  const maxInSeconds = max / 1000;
+
+  const handleOnChange = useCallback(
+    (v: number) => {
+      onChange(v * 1000);
+    },
+    [onChange]
+  );
 
   return (
     <Field
@@ -30,45 +43,28 @@ export const Timeout = ({ checkType }: TimeoutProps) => {
       invalid={Boolean(errors.timeout)}
       label="Timeout"
     >
-      <Controller
-        name="timeout"
-        control={control}
-        render={({ field }) => {
-          const { ref, onChange, value, ...fieldProps } = field; // ref is unused, this is to silence warnings
-          const valueInSeconds = value / 1000;
-          const minInSeconds = min / 1000;
-          const maxInSeconds = max / 1000;
-          const handleOnChange = (v: number) => {
-            onChange(v * 1000);
-          };
-
-          if (readOnly) {
-            return (
-              <Input
-                {...fieldProps}
-                value={valueInSeconds}
-                id={`timeout`}
-                prefix="Every"
-                readOnly
-                suffix="seconds"
-                width={18}
-              />
-            );
-          }
-          return (
-            <TimeSlider
-              disabled={isFormDisabled}
-              min={minInSeconds}
-              max={maxInSeconds}
-              {...fieldProps}
-              value={valueInSeconds}
-              onChange={handleOnChange}
-              analyticsLabel="timeout"
-              ariaLabelForHandle="timeout"
-            />
-          );
-        }}
-      />
+      {readOnly ? (
+        <Input
+          {...fieldProps}
+          value={valueInSeconds}
+          id={`timeout`}
+          prefix="Every"
+          readOnly
+          suffix="seconds"
+          width={18}
+        />
+      ) : (
+        <TimeSlider
+          disabled={isFormDisabled}
+          min={minInSeconds}
+          max={maxInSeconds}
+          {...fieldProps}
+          value={valueInSeconds}
+          onChange={handleOnChange}
+          analyticsLabel="timeout"
+          ariaLabelForHandle="timeout"
+        />
+      )}
     </Field>
   );
 };
