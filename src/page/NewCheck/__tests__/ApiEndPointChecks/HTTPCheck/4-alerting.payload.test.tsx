5947f487812588bb216b9847ba64aca203eeadec
Virginia Cepeda <virginia.cepeda@grafana.com>
2025-06-11 15:46:49 -0300

    fix: add mockFeatureToggles for feature flag overrides in tests
    - also removes the need for TypeScript error suppression comments related to feature flag overrides

--- src/page/NewCheck/__tests__/ApiEndPointChecks/HTTPCheck/4-alerting.payload.test.tsx
+++ src/page/NewCheck/__tests__/ApiEndPointChecks/HTTPCheck/4-alerting.payload.test.tsx
@@ -1,7 +1,6 @@
-import { config } from '@grafana/runtime';
 import { screen } from '@testing-library/react';
 import { PRIVATE_PROBE } from 'test/fixtures/probes';
-import { probeToMetadataProbe } from 'test/utils';
+import { mockFeatureToggles, probeToMetadataProbe } from 'test/utils';
 
 import { CheckType, FeatureName } from 'types';
 import { goToSection, renderNewForm, submitForm } from 'page/__testHelpers__/checkForm';
@@ -22,8 +21,7 @@ describe(`HttpCheck - Section 4 (Alerting) payload`, () => {
   });
 
   it(`can add specific http alerts`, async () => {
-    jest.replaceProperty(config, 'featureToggles', {
-      // @ts-expect-error
+    mockFeatureToggles({
       [FeatureName.AlertsPerCheck]: true,
     });
 
@@ -60,8 +58,7 @@ describe(`HttpCheck - Section 4 (Alerting) payload`, () => {
   });
 
   it(`should display an error message when the threshold is higher than the total amount of executions`, async () => {
-    jest.replaceProperty(config, 'featureToggles', {
-      // @ts-expect-error
+    mockFeatureToggles({
       [FeatureName.AlertsPerCheck]: true,
     });
     const { user } = await renderNewForm(checkType);
--- src/page/NewCheck/__tests__/ApiEndPointChecks/TCPCheck/4-alerting.payload.test.tsx
+++ src/page/NewCheck/__tests__/ApiEndPointChecks/TCPCheck/4-alerting.payload.test.tsx
@@ -1,7 +1,6 @@
-import { config } from '@grafana/runtime';
 import { screen } from '@testing-library/react';
 import { PRIVATE_PROBE } from 'test/fixtures/probes';
-import { probeToMetadataProbe } from 'test/utils';
+import { mockFeatureToggles, probeToMetadataProbe } from 'test/utils';
 
 import { CheckType, FeatureName } from 'types';
 import { goToSection, renderNewForm, submitForm } from 'page/__testHelpers__/checkForm';
@@ -12,8 +11,7 @@ const checkType = CheckType.TCP;
 
 describe(`TCPCheck - Section 4 (Alerting) payload`, () => {
   it(`can add TLS certificate expiry alert when TLS is enabled`, async () => {
-    jest.replaceProperty(config, 'featureToggles', {
-      // @ts-expect-error
+    mockFeatureToggles({
       [FeatureName.AlertsPerCheck]: true,
     });
 
@@ -49,8 +47,7 @@ describe(`TCPCheck - Section 4 (Alerting) payload`, () => {
   });
 
   it(`disables TLS expiry alert and shows warning if TLS is not enabled`, async () => {
-    jest.replaceProperty(config, 'featureToggles', {
-      // @ts-expect-error
+    mockFeatureToggles({
       [FeatureName.AlertsPerCheck]: true,
     });
 
