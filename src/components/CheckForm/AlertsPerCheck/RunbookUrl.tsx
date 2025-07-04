b2c28868ed0c84d6bc217dc81f360df6f893a968
Virginia Cepeda <virginia.cepeda@grafana.com>
2025-07-04 13:05:32 -0300

    feat: add RunbookUrl input to existing alerts

---
+++ src/components/CheckForm/AlertsPerCheck/RunbookUrl.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+import { InlineField, InlineFieldRow, Input, Text, useStyles2 } from '@grafana/ui';
+
+import { CheckAlertType, CheckFormValues } from 'types';
+
+import { useCheckFormContext } from '../CheckFormContext/CheckFormContext';
+import { getAlertItemStyles } from './AlertItem';
+
+interface RunbookUrlProps {
+  alertType: CheckAlertType;
+  selected: boolean;
+}
+
+export const RunbookUrl = ({ alertType, selected }: RunbookUrlProps) => {
+  const { isFormDisabled } = useCheckFormContext();
+  const { control, formState } = useFormContext<CheckFormValues>();
+  const styles = useStyles2(getAlertItemStyles);
+
+  const runbookUrlError = formState.errors?.alerts?.[alertType]?.runbookUrl?.message;
+
+  if (!selected) {
+    return null;
+  }
+
+  return (
+    <InlineFieldRow className={styles.alertRow}>
+      <Text>Runbook URL (optional): </Text>
+      <InlineField
+        htmlFor={`alert-runbook-url-${alertType}`}
+        invalid={!!runbookUrlError}
+        error={runbookUrlError}
+        validationMessageHorizontalOverflow={true}
+        grow={true}
+      >
+        <Controller
+          name={`alerts.${alertType}.runbookUrl`}
+          control={control}
+          render={({ field }) => (
+            <Input
+              {...field}
+              id={`alert-runbook-url-${alertType}`}
+              data-testid={`alert-runbook-url-${alertType}`}
+              placeholder="https://example.com/runbook"
+              type="url"
+              disabled={!selected || isFormDisabled}
+              width={40}
+            />
+          )}
+        />
+      </InlineField>
+    </InlineFieldRow>
+  );
+};
+
