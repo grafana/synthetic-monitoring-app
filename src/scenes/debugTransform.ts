b40a0160a900a5d90f535c4c66e91f3508c07cc7
Chris Bedwell <christopher.bedwell@grafana.com>
2025-07-14 14:24:17 +0100

    fix: lint warnings

--- src/scenes/debugTransform.ts
+++ src/scenes/debugTransform.ts
@@ -10,7 +10,7 @@ import { map } from 'rxjs/operators';
 export const debugTransform: CustomTransformOperator = () => (source: Observable<DataFrame[]>) => {
   return source.pipe(
     map((data: DataFrame[]) => {
-      // eslint-disable-next-line no-console
+       
       console.log(data);
       return data;
     })
