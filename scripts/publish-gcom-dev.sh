62c2cd09d485aa939644569b7d272486ac01ca9e
Virginia Cepeda <virginia.cepeda@grafana.com>
2025-08-08 16:57:41 -0300

    fix: add response info

--- scripts/publish-gcom-dev.sh
+++ scripts/publish-gcom-dev.sh
@@ -15,8 +15,17 @@ fi
 
 DEV_GCS_URL="https://storage.googleapis.com/grafanalabs-synthetic-monitoring-app-dev/builds/grafana-synthetic-monitoring-app-latest.zip"
 
-curl -f -w "status=%{http_code}" -s -H "Authorization: Bearer ${GCOM_PUBLISH_TOKEN}" \
+echo "Making API call..."
+response=$(curl -w "status=%{http_code}" -H "Authorization: Bearer ${GCOM_PUBLISH_TOKEN}" \
 -d "download[any][url]=$DEV_GCS_URL" \
 -d "download[any][md5]=$$(curl -sL $DEV_GCS_URL | md5sum | cut -d' ' -f1)" \
 -d url=https://github.com/grafana/synthetic-monitoring-app \
-"https://grafana-dev.com/api/plugins"
+"https://grafana-dev.com/api/plugins" 2>&1)
+
+echo "Full response:"
+echo "$response"
+
+if [[ "$response" == *"status=500"* ]]; then
+  echo "Server returned 500 error"
+  exit 1
+fi
