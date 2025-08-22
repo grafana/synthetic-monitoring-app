d10c7caf59f3d3ebb9a7ab09c5fde49fb18a1246
Virginia Cepeda <virginia.cepeda@grafana.com>
2025-08-22 16:00:36 -0300

    fix: avoid making the channels request when FF is off

--- src/data/useK6Channels.ts
+++ src/data/useK6Channels.ts
@@ -9,30 +9,30 @@ export const queryKeys: Record<'list' | 'current', (channelId?: string) => Query
   current: (channelId) => ['k6-channel-current', channelId],
 };
 
-const channelsQuery = (api: SMDataSource) => ({
+const channelsQuery = (api: SMDataSource, enabled = true) => ({
   queryKey: queryKeys.list(),
   queryFn: async (): Promise<ListChannelsResponse> => {
     return api.listK6Channels();
   },
-  enabled: true,
+  enabled,
 });
 
-const currentVersionQuery = (api: SMDataSource, channelId: string) => ({
+const currentVersionQuery = (api: SMDataSource, channelId: string, enabled = true) => ({
   queryKey: queryKeys.current(channelId),
   queryFn: async (): Promise<string> => {
     return api.getCurrentK6Version(channelId);
   },
-  enabled: Boolean(channelId),
+  enabled: Boolean(channelId) && enabled,
 });
 
-export function useK6Channels() {
+export function useK6Channels(enabled = true) {
   const smDS = useSMDS();
   
-  return useQuery(channelsQuery(smDS));
+  return useQuery(channelsQuery(smDS, enabled));
 }
 
-export function useCurrentK6Version(channelId?: string) {
+export function useCurrentK6Version(channelId?: string, enabled = true) {
   const smDS = useSMDS();
   
-  return useQuery(currentVersionQuery(smDS, channelId || ''));
+  return useQuery(currentVersionQuery(smDS, channelId || '', enabled));
 }
