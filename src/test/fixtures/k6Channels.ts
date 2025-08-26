86a05a6b4a18fc89a8e13d1f0dabcdd6337eb988
Virginia Cepeda <virginia.cepeda@grafana.com>
2025-08-26 17:27:19 -0300

    fix: add support for disabledAfter prop

--- src/test/fixtures/k6Channels.ts
+++ src/test/fixtures/k6Channels.ts
@@ -5,18 +5,21 @@ export const K6_CHANNELS: Record<string, K6Channel> = {
     name: 'v0',
     default: false,
     deprecatedAfter: '2024-12-31T00:00:00Z', // Already deprecated
+    disabledAfter: '2025-12-31T00:00:00Z',
     manifest: 'k6>=0.5,k6<1',
   },
   v1: {
     name: 'v1',
     default: true,
     deprecatedAfter: '2025-12-31T00:00:00Z',
+    disabledAfter: '2026-12-31T00:00:00Z',
     manifest: 'k6>=1,k6<2',
   },
   v2: {
     name: 'v2',
     default: false,
     deprecatedAfter: '2026-12-31T00:00:00Z',
+    disabledAfter: '2027-12-31T00:00:00Z',
     manifest: 'k6>=2',
   },
 };
