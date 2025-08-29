d0dfe526d13bef4df79734fae017d00faa64b92b
Virginia Cepeda <virginia.cepeda@grafana.com>
2025-08-29 16:09:39 -0300

    fix: add zod validation for selected disabled channels

--- src/types.ts
+++ src/types.ts
@@ -336,6 +336,7 @@ export type CheckAlertFormRecord = Partial<Record<CheckAlertType, CheckAlertForm
 export type CheckFormValuesBase = Omit<Check, 'settings' | 'basicMetricsOnly' | 'alerts'> & {
   publishAdvancedMetrics: boolean;
   alerts?: CheckAlertFormRecord;
+  channelDisabled?: boolean;
 };
 
 export type CheckFormValuesHttp = CheckFormValuesBase & {
