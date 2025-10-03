e72383fd467ecf3030c0d53030166ed39b40aad5
w1kman <thomas@w1kman.se>
2025-10-03 13:08:10 +0200

    feat: check editor refactor
    - fix SectionContent usage

--- src/components/Checkster/components/form/layouts/GenericAlertingSection/GenericAlertingSection.tsx
+++ src/components/Checkster/components/form/layouts/GenericAlertingSection/GenericAlertingSection.tsx
@@ -1,38 +1,35 @@
 import React from 'react';
-import { Stack } from '@grafana/ui';
 
-import { Feedback } from '../../../../../Feedback';
-import { FIELD_SPACING } from '../../../../constants';
+import { Feedback } from 'components/Feedback';
+
+import { SectionContent } from '../../../ui/SectionContent';
 import { FormTabContent, FormTabs } from '../../FormTabs';
 import { GenericAlertingField } from '../../generic/GenericAlertingField';
 import { GenericLegacyAlertingField } from '../../generic/GenericLegacyAlertingField';
 
 export function GenericAlertingSection() {
   return (
-    <>
-      <h2>Alerting</h2>
-      <Stack direction="column" gap={FIELD_SPACING}>
-        <FormTabs>
-          <FormTabContent
-            label="Per-check alerts"
-            actions={
-              <Feedback
-                about={{
-                  text: 'New!',
-                  link: 'https://grafana.com/docs/grafana-cloud/testing/synthetic-monitoring/configure-alerts/configure-per-check-alerts/',
-                  tooltipText: 'Read more about per-check alerts',
-                }}
-                feature="alerts_per_check"
-              />
-            }
-          >
-            <GenericAlertingField />
-          </FormTabContent>
-          <FormTabContent label="Legacy alerts">
-            <GenericLegacyAlertingField />
-          </FormTabContent>
-        </FormTabs>
-      </Stack>
-    </>
+    <SectionContent>
+      <FormTabs>
+        <FormTabContent
+          label="Per-check alerts"
+          actions={
+            <Feedback
+              about={{
+                text: 'New!',
+                link: 'https://grafana.com/docs/grafana-cloud/testing/synthetic-monitoring/configure-alerts/configure-per-check-alerts/',
+                tooltipText: 'Read more about per-check alerts',
+              }}
+              feature="alerts_per_check"
+            />
+          }
+        >
+          <GenericAlertingField />
+        </FormTabContent>
+        <FormTabContent label="Legacy alerts">
+          <GenericLegacyAlertingField />
+        </FormTabContent>
+      </FormTabs>
+    </SectionContent>
   );
 }
--- src/components/Checkster/components/form/layouts/GenericExecutionSection.tsx
+++ src/components/Checkster/components/form/layouts/GenericExecutionSection.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
+import { SectionContent } from '../../ui/SectionContent';
 import { GenericFrequencyField } from '../generic/GenericFrequencyField';
 import { GenericProbesSelectField } from '../generic/GenericProbesSelectField';
 
 export function GenericExecutionSection() {
   return (
-    <>
-      <h2>Execution</h2>
+    <SectionContent>
       <GenericProbesSelectField />
       <GenericFrequencyField />
-    </>
+    </SectionContent>
   );
 }
