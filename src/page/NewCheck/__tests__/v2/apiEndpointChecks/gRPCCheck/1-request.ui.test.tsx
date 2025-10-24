3d8f64b3592ae1cb90cd3694cad75c092cda5c01
w1kman <thomas@w1kman.se>
2025-10-24 15:44:53 +0200

    chore: add tests for `NewCheckV2.tsx`
    - chore: add tests `gRPC`

---
+++ src/page/NewCheck/__tests__/v2/apiEndpointChecks/gRPCCheck/1-request.ui.test.tsx
@@ -0,0 +1,26 @@
+import { screen } from '@testing-library/react';
+
+import { CheckType } from 'types';
+import { renderNewFormV2 } from 'page/__testHelpers__/checkForm';
+
+import { submitForm } from '../../../../../../components/Checkster/__testHelpers__/formHelpers';
+import { fillMandatoryFields } from '../../utils';
+
+const checkType = CheckType.GRPC;
+
+describe(`gRPCCheck - Section 1 (Request) UI`, () => {
+  it(`will navigate to the first section and open the request to reveal a nested error`, async () => {
+    const { user } = await renderNewFormV2(checkType);
+    await user.click(screen.getByText('Request options'));
+    await user.click(screen.getByRole('tab', { name: 'TLS' }));
+
+    const certInputPreSubmit = screen.getByLabelText('CA certificate', { exact: false });
+    await user.type(certInputPreSubmit, `not a cert`);
+
+    await fillMandatoryFields({ user, checkType });
+    await submitForm(user);
+
+    const err = await screen.findByText(`Certificate must be in the PEM format.`);
+    expect(err).toBeInTheDocument();
+  });
+});
