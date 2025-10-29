c788696b30188ff1384acc0a4b139630905e7ffa
w1kman <thomas@w1kman.se>
2025-10-29 12:14:20 +0100

    fix: incorrect handling of onSubmit for new/existing check

--- src/hooks/useHandleSubmitCheckster.ts
+++ src/hooks/useHandleSubmitCheckster.ts
@@ -45,7 +45,7 @@ export function useHandleSubmitCheckster(initialCheck?: Check) {
       await handleAlerts(result, alertsEnabled ? formValues.alerts : undefined);
       await queryClient.invalidateQueries({ queryKey: queryKeys.list });
 
-      return () => navigateToCheckDashboard(result, payload?.id !== undefined);
+      return () => navigateToCheckDashboard(result, payload?.id === undefined);
     },
     [alertsEnabled, initialCheck, createCheck, handleAlerts, navigateToCheckDashboard, updateCheck]
   );
