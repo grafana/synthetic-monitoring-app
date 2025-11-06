2bf62cd9a34ba29af5fa884011efc9c18ea4f1f4
Chris Bedwell <christopher.bedwell@grafana.com>
2025-11-06 15:55:36 +0000

    fix: render nothing if no msg or error in log line

--- src/components/Checkster/feature/adhoc-check/LogMessage.tsx
+++ src/components/Checkster/feature/adhoc-check/LogMessage.tsx
@@ -29,6 +29,11 @@ export function LogMessage({ log, logLevel }: { log: LogEntry; logLevel: string 
 
   const iconName = getMsgIconFromLog(log);
   const upgrade = iconName === 'user' && log.level !== 'info'; // since there is no differentiating between console.log and console.info
+  const logLine = log.msg ? getMsgFromLogMsg(log.msg) : log.error ? log.error : undefined;
+
+  if (!logLine) {
+    return null;
+  }
 
   return (
     <span
@@ -40,7 +45,7 @@ export function LogMessage({ log, logLevel }: { log: LogEntry; logLevel: string 
       `}
     >
       {iconName !== undefined && <Icon name={iconName} />}
-      {log.msg ? getMsgFromLogMsg(log.msg) : log.error ? log.error : undefined}
+      {logLine}
     </span>
   );
 }
