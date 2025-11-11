c12f6278932cb91d8c5ac9a1b8e76d317f910b05
w1kman <thomas@w1kman.se>
2025-11-11 18:41:33 +0100

    chore: remove old check editor
    - remove unused code

--- src/components/CheckEditor/transformations/toFormValues.utils.ts
+++ src/components/CheckEditor/transformations/toFormValues.utils.ts
@@ -1,44 +1,10 @@
-import { SelectableValue } from '@grafana/data';
-
-import { Check, CheckAlertPublished, CheckFormValues, TLSConfig } from 'types';
-import { fromBase64 } from 'utils';
+import { Check, CheckAlertPublished, CheckFormValues } from 'types';
 import {
   GLOBAL_PREDEFINED_ALERTS,
   PredefinedAlertInterface,
 } from 'components/CheckForm/AlertsPerCheck/AlertsPerCheck.constants';
 
 import { getCheckAlertsFormValues } from './toFormValues.alerts';
-
-export function selectableValueFrom<T>(value: T, label?: string): SelectableValue<T> {
-  const labelValue = String(value);
-  return { label: label ?? labelValue, value };
-}
-
-export const getTlsConfigFormValues = (tlsConfig?: TLSConfig) => {
-  if (!tlsConfig || Object.keys(tlsConfig).length === 0) {
-    return {};
-  }
-
-  return {
-    tlsConfig: {
-      ...tlsConfig,
-      caCert: getDecodedIfPEM(tlsConfig.caCert),
-      clientCert: getDecodedIfPEM(tlsConfig.clientCert),
-      clientKey: getDecodedIfPEM(tlsConfig.clientKey),
-    },
-  };
-};
-
-const getDecodedIfPEM = (cert = '') => {
-  const decoded = fromBase64(cert);
-  if (decoded === undefined) {
-    return cert;
-  }
-  if (decoded.indexOf('BEGIN') > 0) {
-    return decoded;
-  }
-  return cert;
-};
 
 export function getBaseFormValuesFromCheck(check: Check): Omit<CheckFormValues, 'checkType' | 'settings'> {
   return {
