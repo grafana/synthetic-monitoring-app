bbaed0fe57a626ce1935874ababd38e1630b2c22
Virginia Cepeda <virginia.cepeda@grafana.com>
2025-11-19 14:59:48 -0300

    fix: remove semantic commit scopes from Renovate to comply with commi… (#1469)
    * fix: remove semantic commit scopes from Renovate to comply with commitlint
    
    * fix: add release type to commitlint for release-please compatibility

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,1 +1,24 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Add 'release' type to the conventional commit types
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'release', // Added for release-please
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
+};
