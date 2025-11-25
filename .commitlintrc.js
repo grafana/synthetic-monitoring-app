4e0257832052b5ac406f122f52064a6d217eeb22
Chris Bedwell <christopher.bedwell@grafana.com>
2025-11-25 09:55:27 +0000

    fix: unify our commitlint and release please expectations

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,24 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    // Add 'release' type to the conventional commit types
-    'type-enum': [
-      2,
-      'always',
-      [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'release', // Added for release-please
-        'revert',
-        'style',
-        'test',
-      ],
-    ],
+    // Match the types defined in release-please-config.json
+    'type-enum': [2, 'always', ['chore', 'docs', 'feat', 'fix', 'refactor', 'release']],
   },
 };
