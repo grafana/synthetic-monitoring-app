f7fe8b7d232954b89da281546738e7aac1d45558
Chris Bedwell <christopher.bedwell@grafana.com>
2025-11-27 11:49:54 +0000

    Feat: highlight docs panel (#1444)
    * feat: add k6 script information banner to check editors
    
    - add dismissable info banner explaining k6 scripts to scripted and browser check editors
    - banner includes links to k6 documentation and k6 Studio
    - users can dismiss the banner by clicking X button
    - browser checks link to k6-browser docs, scripted checks link to k6 docs
    
    * feat: add k6 Studio callout to new Checkster editor and fix spacing
    
    - add k6 Studio information banner to ScriptedCheckContent
    - banner supports both Scripted and Browser checks in new editor
    - fix excessive spacing between banner and code editor
    - banner dynamically shows correct documentation link based on check type
    
    * feat: wip
    
    * feat: add custom documentation depending on what check you are viewing
    
    * feat: add focus styles when clicking on About k6 scripts
    
    * feat: add analytics to docslinks
    
    * feat: generate tracking event and update property name
    
    * feat: track need help writing scripts button
    
    * feat: working on tests before implementing rest of feedback
    
    * feat: added tests before refactoring
    
    * fix: feedback and added appropriate tests
    
    * feat: added fading box shadow to docs panel
    
    ---------
    
    Co-authored-by: Mark Meier <11074462+markjmeier@users.noreply.github.com>

--- src/components/Checkster/components/ui/SecondaryLayoutSection.tsx
+++ src/components/Checkster/components/ui/SecondaryLayoutSection.tsx
@@ -1,9 +1,10 @@
 import React, { PropsWithChildren, ReactNode } from 'react';
 import { GrafanaTheme2 } from '@grafana/data';
-import { useStyles2 } from '@grafana/ui';
+import { styleMixins, useStyles2 } from '@grafana/ui';
 import { css, cx } from '@emotion/css';
 
 import { SECONDARY_CONTAINER_ID } from 'components/Checkster/constants';
+import { useFeatureTabsContext } from 'components/Checkster/contexts/FeatureTabsContext';
 
 import { useAppContainerContext } from '../../contexts/AppContainerContext';
 import { LayoutSectionContent } from './LayoutSectionContent';
@@ -19,13 +20,21 @@ export function SecondaryLayoutSection({ children, headerContent }: SecondaryLay
     secondaryProps: { className: secondaryClassname, ...secondaryProps },
     splitterProps: { className: splitterClassname, ...splitterProps },
   } = useAppContainerContext();
+  const { highlightedTab, activeTab } = useFeatureTabsContext();
 
   return (
     <>
       <div className={cx(splitterClassname, styles.splitter)} {...splitterProps} />
       <div className={cx(secondaryClassname, styles.secondary)} {...secondaryProps}>
         <LayoutSectionHeader>{headerContent}</LayoutSectionHeader>
-        <LayoutSectionContent id={SECONDARY_CONTAINER_ID} tabIndex={0}>
+        <LayoutSectionContent
+          className={cx(styles.secondaryContent, {
+            [styles.highlighted]: highlightedTab === activeTab[0],
+          })}
+          id={SECONDARY_CONTAINER_ID}
+          key={activeTab[0]} // Force re-render when active tab changes -- clears the highlight
+          tabIndex={0}
+        >
           {children}
         </LayoutSectionContent>
       </div>
@@ -43,6 +52,15 @@ function getStyles(theme: GrafanaTheme2) {
       &::before {
         border-right: 1px solid ${theme.colors.border.medium};
       }
+    `,
+    secondaryContent: css`
+      /* This is needed to avoid the box-shadow from being cut off */
+      margin: 4px 4px 4px 0;
+      transition: box-shadow 5s ease-in-out;
+    `,
+    highlighted: css`
+      ${styleMixins.getFocusStyles(theme)}
+      transition: box-shadow 0s;
     `,
   };
 }
