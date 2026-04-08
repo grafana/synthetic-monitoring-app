248032c90940645a01b5718dab7e1c861176a1f9
John Lacuna <lacunajo@gmail.com>
2026-04-08 09:11:20 -0400

    fix: mock service worker (#1648)

--- src/startServerWorker.ts
+++ src/startServerWorker.ts
@@ -1,4 +1,22 @@
-import { setupWorker } from 'msw/browser';
+import { BatchInterceptor } from '@mswjs/interceptors';
+import { FetchInterceptor } from '@mswjs/interceptors/fetch';
+import { XMLHttpRequestInterceptor } from '@mswjs/interceptors/XMLHttpRequest';
 import { HANDLERS } from 'test/handlers';
 
-setupWorker(...HANDLERS).start();
+const interceptor = new BatchInterceptor({
+  name: 'msw-interceptor',
+  interceptors: [new FetchInterceptor(), new XMLHttpRequestInterceptor()],
+});
+
+interceptor.apply();
+
+interceptor.on('request', async ({ request, requestId, controller }) => {
+  for (const handler of HANDLERS) {
+    const result = await handler.run({ request: request.clone(), requestId });
+
+    if (result?.response) {
+      controller.respondWith(result.response);
+      return;
+    }
+  }
+});
